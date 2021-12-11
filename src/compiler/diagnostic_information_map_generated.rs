12e886570668489364caac3c5ef9045a0721c6a4
Julian Rosse <julian@helixbass.net>
2021-12-11 14:57:09 -0500

    const diag() helper

--- src/compiler/diagnostic_information_map_generated.rs
+++ src/compiler/diagnostic_information_map_generated.rs
@@ -1,13 +1,27 @@
 use crate::{DiagnosticCategory, DiagnosticMessage};
 
+const fn diag(
+    code: u32,
+    category: DiagnosticCategory,
+    key: &'static str,
+    message: &'static str,
+) -> DiagnosticMessage {
+    DiagnosticMessage {
+        code,
+        category,
+        key,
+        message,
+    }
+}
+
 #[non_exhaustive]
 pub struct Diagnostics;
 #[allow(non_upper_case_globals)]
 impl Diagnostics {
-    pub const _0_expected: DiagnosticMessage = DiagnosticMessage {
-        code: 1005,
-        category: DiagnosticCategory::Error,
-        key: "_0_expected_1005",
-        message: "'{0}' expected.",
-    };
+    pub const _0_expected: DiagnosticMessage = diag(
+        1005,
+        DiagnosticCategory::Error,
+        "_0_expected_1005",
+        "'{0}' expected.",
+    );
 }
