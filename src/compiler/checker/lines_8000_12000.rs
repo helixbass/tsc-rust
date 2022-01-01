e392503c105355058730948b5ef872d896796120
Julian Rosse <julian@helixbass.net>
2022-01-01 11:41:37 -0500

    rm unused

--- src/compiler/checker/lines_8000_12000.rs
+++ src/compiler/checker/lines_8000_12000.rs
@@ -1,39 +1,16 @@
 #![allow(non_upper_case_globals)]
 
-use bitflags::bitflags;
-use std::borrow::Borrow;
-use std::cell::{Ref, RefCell, RefMut};
-use std::collections::{HashMap, HashSet};
-use std::ptr;
+use std::cell::RefCell;
 use std::rc::Rc;
 
 use super::NodeBuilderContext;
 use crate::{
-    TypeNode, UnionOrIntersectionType, UnionOrIntersectionTypeInterface, UnionType,
-    VariableDeclaration, VariableStatement, __String, bind_source_file, chain_diagnostic_messages,
-    create_diagnostic_collection, create_diagnostic_for_node,
-    create_diagnostic_for_node_from_message_chain, create_printer, create_symbol_table,
-    create_text_writer, declaration_name_to_string, escape_leading_underscores, every, factory,
-    first_defined, first_or_undefined, for_each, get_effective_initializer,
-    get_effective_type_annotation_node, get_first_identifier, get_name_of_declaration,
-    get_object_flags, get_source_file_of_node, get_synthetic_factory, has_dynamic_name,
-    has_initializer, is_binding_element, is_external_or_common_js_module, is_identifier_text,
-    is_object_literal_expression, is_private_identifier, is_property_assignment,
-    is_property_declaration, is_property_signature, is_variable_declaration, node_is_missing,
-    object_allocator, unescape_leading_underscores, using_single_line_string_writer, ArrayTypeNode,
-    BaseInterfaceType, BaseIntrinsicType, BaseLiteralType, BaseNodeFactorySynthetic,
-    BaseObjectType, BaseType, BaseUnionOrIntersectionType, CharacterCodes, Debug_, Diagnostic,
-    DiagnosticCollection, DiagnosticMessage, DiagnosticMessageChain, Diagnostics, EmitHint,
-    EmitTextWriter, Expression, ExpressionStatement, FreshableIntrinsicType,
-    HasExpressionInitializerInterface, InterfaceDeclaration, InterfaceType, IntrinsicType,
-    KeywordTypeNode, LiteralLikeNode, LiteralLikeNodeInterface, LiteralType, LiteralTypeInterface,
-    NamedDeclarationInterface, Node, NodeInterface, Number, NumberLiteralType, NumericLiteral,
-    ObjectFlags, ObjectFlagsTypeInterface, ObjectLiteralExpression, PrefixUnaryExpression,
-    PrinterOptions, PropertyAssignment, PropertySignature, RelationComparisonResult,
-    ResolvableTypeInterface, ResolvedTypeInterface, SourceFile, Statement, StringLiteralType,
-    Symbol, SymbolFlags, SymbolFormatFlags, SymbolTable, SymbolTracker, SyntaxKind, Ternary, Type,
-    TypeChecker, TypeCheckerHost, TypeElement, TypeFlags, TypeInterface, TypeReferenceNode,
-    UnionReduction, VariableLikeDeclarationInterface,
+    __String, declaration_name_to_string, escape_leading_underscores, first_defined,
+    get_effective_type_annotation_node, get_name_of_declaration, has_dynamic_name,
+    is_property_assignment, is_property_declaration, is_property_signature,
+    is_variable_declaration, BaseInterfaceType, Debug_, InterfaceType, LiteralType, Node,
+    NodeInterface, ObjectFlags, ObjectFlagsTypeInterface, Symbol, SymbolFlags, SymbolTable, Type,
+    TypeChecker, TypeFlags, TypeInterface,
 };
 
 impl TypeChecker {
