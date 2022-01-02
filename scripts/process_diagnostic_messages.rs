bd587e13e012507e62ddfeb0c932172cd65663f2
Julian Rosse <julian@helixbass.net>
2022-01-02 10:44:07 -0500

    parse with serde

--- scripts/process_diagnostic_messages.rs
+++ scripts/process_diagnostic_messages.rs
@@ -1,12 +1,41 @@
 #!/usr/bin/env rust-script
+//!
+//! ```cargo
+//! [dependencies]
+//! serde = { version = "1.0", features = ["derive"] }
+//! serde_json = "1.0"
+//! ```
 
+use serde::Deserialize;
+use std::collections::HashMap;
 use std::env;
+use std::fs;
+
+#[derive(Debug, Deserialize)]
+enum DiagnosticCategory {
+    Error,
+    Message,
+    Suggestion,
+}
+
+#[derive(Debug, Deserialize)]
+struct DiagnosticMessageSpec {
+    category: DiagnosticCategory,
+    code: u32,
+}
 
 fn main() {
-    let args = env::args();
+    let args = env::args().collect::<Vec<_>>();
     if args.len() < 2 {
-        eprintln!("Usage:");
-        eprintln!("\t./scripts/process_diagnostic_messages.rs <diagnostic-json-input-file>");
+        println!("Usage:");
+        println!("\t./scripts/process_diagnostic_messages.rs <diagnostic-json-input-file>");
         return;
     }
+    let input_file_path = &args[1]/*.replace(/\\/g, "/")*/;
+    println!("Reading diagnostics from {}", input_file_path);
+    let input_str = fs::read_to_string(input_file_path).expect("Couldn't read input file");
+    let diagnostic_messages_json: HashMap<String, DiagnosticMessageSpec> =
+        serde_json::from_str(&input_str).expect("Couldn't parse input file as JSON");
 }
+
+// fn write_file(input_file_path: )
