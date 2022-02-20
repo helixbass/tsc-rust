f21d5bb4df4af41f111fd4f17625b9e972e52f82
Julian Rosse <julian@helixbass.net>
2022-02-20 16:26:00 -0500

    get trailing semicolon deferring writer

--- src/compiler/utilities/lines_4000_5000.rs
+++ src/compiler/utilities/lines_4000_5000.rs
@@ -267,6 +267,153 @@ pub fn create_text_writer(new_line: &str) -> TextWriter {
     // text_writer.reset()
 }
 
+pub fn get_trailing_semicolon_deferring_writer<TWriter: EmitTextWriter>(
+    writer: TWriter,
+) -> TrailingSemicolonDeferringWriter<TWriter> {
+    TrailingSemicolonDeferringWriter::new(writer)
+}
+
+pub struct TrailingSemicolonDeferringWriter<TWriter: EmitTextWriter> {
+    writer: TWriter,
+    pending_trailing_semicolon: bool,
+}
+
+impl<TWriter: EmitTextWriter> TrailingSemicolonDeferringWriter<TWriter> {
+    pub fn new(writer: TWriter) -> Self {
+        Self {
+            writer,
+            pending_trailing_semicolon: false,
+        }
+    }
+
+    fn commit_pending_trailing_semicolon(&mut self) {
+        if self.pending_trailing_semicolon {
+            self.writer.write_trailing_semicolon(";");
+            self.pending_trailing_semicolon = false;
+        }
+    }
+}
+
+impl<TWriter: EmitTextWriter> EmitTextWriter for TrailingSemicolonDeferringWriter<TWriter> {
+    fn write(&mut self, s: &str) {
+        self.writer.write(s)
+    }
+
+    fn write_comment(&mut self, s: &str) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_comment(s)
+    }
+
+    fn raw_write(&mut self, s: &str) {
+        self.writer.raw_write(s)
+    }
+
+    fn write_literal(&mut self, s: &str) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_literal(s)
+    }
+
+    fn write_trailing_semicolon(&mut self, text: &str) {
+        self.pending_trailing_semicolon = true;
+    }
+
+    fn get_text(&self) -> String {
+        self.writer.get_text()
+    }
+
+    fn get_text_pos(&self) -> usize {
+        self.writer.get_text_pos()
+    }
+
+    fn get_line(&self) -> usize {
+        self.writer.get_line()
+    }
+
+    fn get_column(&self) -> usize {
+        self.writer.get_column()
+    }
+
+    fn get_indent(&self) -> usize {
+        self.writer.get_indent()
+    }
+
+    fn is_at_start_of_line(&self) -> bool {
+        self.writer.is_at_start_of_line()
+    }
+
+    fn has_trailing_comment(&self) -> bool {
+        self.writer.has_trailing_comment()
+    }
+
+    fn has_trailing_whitespace(&self) -> bool {
+        self.writer.has_trailing_whitespace()
+    }
+}
+
+impl<TWriter: EmitTextWriter> SymbolWriter for TrailingSemicolonDeferringWriter<TWriter> {
+    fn write_line(&mut self, _force: Option<bool>) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_line(None)
+    }
+
+    fn increase_indent(&mut self) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.increase_indent()
+    }
+
+    fn decrease_indent(&mut self) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.decrease_indent()
+    }
+
+    fn write_keyword(&mut self, s: &str) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_keyword(s)
+    }
+
+    fn write_operator(&mut self, s: &str) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_operator(s)
+    }
+
+    fn write_parameter(&mut self, s: &str) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_parameter(s)
+    }
+
+    fn write_punctuation(&mut self, s: &str) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_punctuation(s)
+    }
+
+    fn write_space(&mut self, s: &str) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_space(s)
+    }
+
+    fn write_string_literal(&mut self, s: &str) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_string_literal(s)
+    }
+
+    fn write_property(&mut self, s: &str) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_property(s)
+    }
+
+    fn write_symbol(&mut self, s: &str, sym: &Symbol) {
+        self.commit_pending_trailing_semicolon();
+        self.writer.write_symbol(s, sym)
+    }
+
+    fn clear(&mut self) {
+        self.writer.clear()
+    }
+}
+
+// TODO: should explicitly forward all SymbolTracker methods to self.writer too?
+impl<TWriter: EmitTextWriter> SymbolTracker for TrailingSemicolonDeferringWriter<TWriter> {}
+
 pub fn get_first_constructor_with_body(
     node: &Node, /*ClassLikeDeclaration*/
 ) -> Option<Rc<Node /*ConstructorDeclaration & { body: FunctionBody }*/>> {
