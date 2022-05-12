5dfa6374a0d53187e82806d42eba55332915de82
Julian Rosse <julian@helixbass.net>
2022-05-12 16:56:21 -0400

    split file

---
+++ src/compiler/checker/lines_15000_16000.rs
@@ -0,0 +1,191 @@
+#![allow(non_upper_case_globals)]
+
+use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::convert::TryInto;
+use std::ptr;
+use std::rc::Rc;
+
+use crate::{
+    concatenate, contains_rc, every, filter, find_index,
+    get_declaration_modifier_flags_from_symbol, get_object_flags, index_of_rc,
+    is_computed_property_name, is_identifier, is_known_symbol, is_private_identifier, map,
+    ordered_remove_item_at, reduce_left, replace_element, some, symbol_name,
+    walk_up_parenthesized_types, AccessFlags, BaseUnionOrIntersectionType, Debug_, Diagnostics,
+    IndexInfo, IndexType, InternalSymbolName, IntersectionType, ModifierFlags, NodeInterface,
+    ObjectFlags, ObjectTypeInterface, SyntaxKind, UnionOrIntersectionTypeInterface, UnionReduction,
+    __String, get_name_of_declaration, unescape_leading_underscores, Node, Symbol, SymbolInterface,
+    Type, TypeChecker, TypeFlags, TypeInterface,
+};
+
+impl TypeChecker {
+    pub(super) fn get_template_string_for_type(&self, type_: &Type) -> Option<String> {
+        unimplemented!()
+    }
+
+    pub(super) fn create_template_literal_type(
+        &self,
+        texts: Vec<String>,
+        types: Vec<Rc<Type>>,
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_string_mapping_type(&self, symbol: &Symbol, type_: &Type) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_property_name_from_index(&self, index_type: &Type) -> Option<__String> {
+        if self.is_type_usable_as_property_name(index_type) {
+            Some(self.get_property_name_from_type(index_type))
+        } else {
+            unimplemented!()
+        }
+    }
+
+    pub(super) fn get_property_type_for_index_type(
+        &self,
+        original_object_type: &Type,
+        object_type: &Type,
+        index_type: &Type,
+        full_index_type: &Type,
+    ) -> Option<Rc<Type>> {
+        let prop_name = if false {
+            unimplemented!()
+        } else {
+            self.get_property_name_from_index(index_type)
+        };
+        if let Some(prop_name) = prop_name {
+            let prop = self.get_property_of_type_(object_type, &prop_name, None);
+            if let Some(prop) = prop {
+                let prop_type = self.get_type_of_symbol(&*prop);
+                return if false {
+                    unimplemented!()
+                } else {
+                    Some(prop_type)
+                };
+            }
+        }
+        None
+    }
+
+    pub(super) fn is_pattern_literal_placeholder_type(&self, type_: &Type) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_pattern_literal_type(&self, type_: &Type) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_generic_type(&self, type_: &Type) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_generic_object_type(&self, type_: &Type) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_generic_index_type(&self, type_: &Type) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_this_type_parameter(&self, type_: &Type) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn get_simplified_type(&self, type_: &Type, writing: bool) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_indexed_access_type<
+        TAccessNode: Borrow<Node>,
+        TAliasSymbol: Borrow<Symbol>,
+    >(
+        &self,
+        object_type: &Type,
+        index_type: &Type,
+        access_flags: Option<AccessFlags>,
+        access_node: Option<
+            TAccessNode, /*ElementAccessExpression | IndexedAccessTypeNode | PropertyName | BindingName | SyntheticExpression*/
+        >,
+        alias_symbol: Option<TAliasSymbol>,
+        alias_type_arguments: Option<&[Rc<Type>]>,
+    ) -> Rc<Type> {
+        let access_flags = access_flags.unwrap_or(AccessFlags::None);
+        unimplemented!()
+    }
+
+    pub(super) fn get_indexed_access_type_or_undefined<
+        TAccessNode: Borrow<Node>,
+        TAliasSymbol: Borrow<Symbol>,
+    >(
+        &self,
+        object_type: &Type,
+        index_type: &Type,
+        access_flags: Option<AccessFlags>,
+        access_node: Option<
+            TAccessNode, /*ElementAccessExpression | IndexedAccessTypeNode | PropertyName | BindingName | SyntheticExpression*/
+        >,
+        alias_symbol: Option<TAliasSymbol>,
+        alias_type_arguments: Option<&[Rc<Type>]>,
+    ) -> Option<Rc<Type>> {
+        let access_flags = access_flags.unwrap_or(AccessFlags::None);
+        let apparent_object_type = self.get_reduced_apparent_type(object_type);
+        self.get_property_type_for_index_type(
+            object_type,
+            &apparent_object_type,
+            index_type,
+            index_type,
+        )
+    }
+
+    pub(super) fn get_type_from_mapped_type_node(
+        &self,
+        node: &Node, /*MappedTypeNode*/
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_actual_type_variable(&self, type_: &Type) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_true_type_from_conditional_type(
+        &self,
+        type_: &Type, /*ConditionalType*/
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_false_type_from_conditional_type(
+        &self,
+        type_: &Type, /*ConditionalType*/
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_inferred_true_type_from_conditional_type(
+        &self,
+        type_: &Type, /*ConditionalType*/
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_infer_type_parameters(
+        &self,
+        node: &Node, /*ConditionalTypeNode*/
+    ) -> Option<Vec<Rc<Type /*TypeParameter*/>>> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_alias_symbol_for_type_node(&self, node: &Node) -> Option<Rc<Symbol>> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_arguments_for_alias_symbol<TSymbol: Borrow<Symbol>>(
+        &self,
+        symbol: Option<TSymbol>,
+    ) -> Option<Vec<Rc<Type>>> {
+        unimplemented!()
+    }
+}
