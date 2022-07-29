83a1577ac052f108affa8196d0119127d6165fef
Julian Rosse <julian@helixbass.net>
2022-07-29 18:52:19 -0400

    split file

---
+++ src/compiler/checker/lines_19000_20000.rs
@@ -0,0 +1,199 @@
+#![allow(non_upper_case_globals)]
+
+use regex::{Captures, Regex};
+use std::collections::HashSet;
+use std::ptr;
+use std::rc::Rc;
+
+use super::{
+    CheckTypeContainingMessageChain, CheckTypeRelatedTo, ExpandingFlags, IntersectionState,
+    MappedTypeModifiers, RecursionFlags,
+};
+use crate::{
+    are_option_rcs_equal, are_rc_slices_equal, get_object_flags, same_map, AccessFlags,
+    DiagnosticMessageChain, Diagnostics, InferenceFlags, InferencePriority, Node, NodeInterface,
+    ObjectFlags, ObjectTypeInterface, RelationComparisonResult, Signature, SignatureKind, Symbol,
+    SymbolInterface, Ternary, Type, TypeChecker, TypeFlags, TypeInterface, TypeMapper,
+    TypeMapperCallback, UnionOrIntersectionTypeInterface, VarianceFlags, __String,
+};
+
+impl<'type_checker, TContainingMessageChain: CheckTypeContainingMessageChain>
+    CheckTypeRelatedTo<'type_checker, TContainingMessageChain>
+{
+    pub(super) fn count_message_chain_breadth(
+        &self,
+        info: Option<&[&DiagnosticMessageChain]>,
+    ) -> usize {
+        unimplemented!()
+    }
+
+    pub(super) fn relate_variances(
+        &self,
+        source_type_arguments: Option<&[Rc<Type>]>,
+        target_type_arguments: Option<&[Rc<Type>]>,
+        variances: &[VarianceFlags],
+        intersection_state: IntersectionState,
+    ) -> Option<Ternary> {
+        unimplemented!()
+    }
+
+    pub(super) fn mapped_type_related_to(
+        &self,
+        source: &Type, /*MappedType*/
+        target: &Type, /*MappedType*/
+        report_errors: bool,
+    ) -> Ternary {
+        unimplemented!()
+    }
+
+    pub(super) fn type_related_to_discriminated_type(
+        &self,
+        source: &Type,
+        target: &Type, /*UnionType*/
+    ) -> Ternary {
+        unimplemented!()
+    }
+
+    pub(super) fn exclude_properties(
+        &self,
+        properties: Vec<Rc<Symbol>>,
+        excluded_properties: Option<HashSet<__String>>,
+    ) -> Vec<Rc<Symbol>> {
+        properties
+    }
+
+    pub(super) fn is_property_symbol_type_related(
+        &self,
+        source_prop: &Symbol,
+        target_prop: &Symbol,
+        get_type_of_source_property: fn(&TypeChecker, &Symbol) -> Rc<Type>,
+        report_errors: bool,
+        intersection_state: IntersectionState,
+    ) -> Ternary {
+        let target_is_optional = false;
+        let effective_target = self.type_checker.add_optionality(
+            &self
+                .type_checker
+                .get_non_missing_type_of_symbol(target_prop),
+            Some(false),
+            Some(target_is_optional),
+        );
+        let effective_source = get_type_of_source_property(self.type_checker, source_prop);
+        self.is_related_to(
+            &effective_source,
+            &effective_target,
+            Some(RecursionFlags::Both),
+            Some(report_errors),
+            None,
+            Some(intersection_state),
+        )
+    }
+
+    pub(super) fn property_related_to(
+        &self,
+        source: &Type,
+        target: &Type,
+        source_prop: &Symbol,
+        target_prop: &Symbol,
+        get_type_of_source_property: fn(&TypeChecker, &Symbol) -> Rc<Type>,
+        report_errors: bool,
+        intersection_state: IntersectionState,
+        skip_optional: bool,
+    ) -> Ternary {
+        let related = self.is_property_symbol_type_related(
+            source_prop,
+            target_prop,
+            get_type_of_source_property,
+            report_errors,
+            intersection_state,
+        );
+        if related == Ternary::False {
+            if report_errors {
+                self.report_incompatible_error(
+                    &Diagnostics::Types_of_property_0_are_incompatible,
+                    Some(vec![self.type_checker.symbol_to_string_(
+                        target_prop,
+                        Option::<&Node>::None,
+                        None,
+                        None,
+                        None,
+                    )]),
+                );
+            }
+            return Ternary::False;
+        }
+        related
+    }
+
+    pub(super) fn properties_related_to(
+        &self,
+        source: &Type,
+        target: &Type,
+        report_errors: bool,
+        excluded_properties: Option<HashSet<__String>>,
+        intersection_state: IntersectionState,
+    ) -> Ternary {
+        let mut result = Ternary::True;
+        let require_optional_properties = false;
+        // let unmatched_property =
+        //     self.get_unmatched_property(source, target, require_optional_properties, false);
+        // if let Some(unmatched_property) = unmatched_property {
+        //     if report_errors {
+        //         self.report_unmatched_property(
+        //             source,
+        //             target,
+        //             unmatched_property,
+        //             require_optional_properties,
+        //         );
+        //     }
+        //     return Ternary::False;
+        // }
+        let properties = self.type_checker.get_properties_of_type(target);
+        for target_prop in self.exclude_properties(properties, excluded_properties) {
+            let name = target_prop.escaped_name();
+            if true {
+                let source_prop = self.type_checker.get_property_of_type_(source, name, None);
+                if let Some(source_prop) = source_prop {
+                    if !Rc::ptr_eq(&source_prop, &target_prop) {
+                        let related = self.property_related_to(
+                            source,
+                            target,
+                            &source_prop,
+                            &target_prop,
+                            TypeChecker::get_non_missing_type_of_symbol,
+                            report_errors,
+                            intersection_state,
+                            true,
+                        );
+                        if related == Ternary::False {
+                            return Ternary::False;
+                        }
+                        result &= related;
+                    }
+                }
+            }
+        }
+        result
+    }
+
+    pub(super) fn signatures_related_to(
+        &self,
+        source: &Type,
+        target: &Type,
+        kind: SignatureKind,
+        report_errors: bool,
+    ) -> Ternary {
+        unimplemented!()
+    }
+
+    pub(super) fn index_signatures_related_to(
+        &self,
+        source: &Type,
+        target: &Type,
+        source_is_primitive: bool,
+        report_errors: bool,
+        intersection_state: IntersectionState,
+    ) -> Ternary {
+        unimplemented!()
+    }
+}
