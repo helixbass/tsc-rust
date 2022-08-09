9bbd4eb8b3d2572599694381577b5233c55810ee
Julian Rosse <julian@helixbass.net>
2022-08-09 12:20:30 -0400

    split file

---
+++ src/compiler/checker/lines_21500_24000.rs
@@ -0,0 +1,333 @@
+#![allow(non_upper_case_globals)]
+
+use std::borrow::Borrow;
+use std::cell::RefCell;
+use std::cmp;
+use std::collections::HashMap;
+use std::ptr;
+use std::rc::Rc;
+
+use super::{MappedTypeModifiers, TypeFacts, WideningKind};
+use crate::{
+    create_symbol_table, escape_leading_underscores, filter, find_ancestor,
+    get_declaration_of_kind, is_function_type_node, is_identifier, is_method_signature, map,
+    same_map, some, ElementFlags, FindAncestorCallbackReturn, IndexInfo, NamedDeclarationInterface,
+    ReverseMappedType, SymbolInterface, SyntaxKind, TransientSymbolInterface, TypeComparer,
+    TypeMapper, TypeMapperCallback, TypeReferenceInterface, __String, declaration_name_to_string,
+    for_each_bool, get_name_of_declaration, get_object_flags, get_source_file_of_node,
+    is_call_signature_declaration, is_check_js_enabled_for_file, is_in_js_file, is_type_node_kind,
+    is_write_only_access, node_is_missing, DiagnosticMessage, Diagnostics, InferenceContext,
+    InferenceFlags, InferenceInfo, InferencePriority, Node, NodeInterface, ObjectFlags, Signature,
+    Symbol, SymbolFlags, Ternary, Type, TypeChecker, TypeFlags, TypeInterface, UnionReduction,
+    WideningContext,
+};
+
+impl TypeChecker {
+    pub(super) fn infer_reverse_mapped_type(
+        &self,
+        source_type: &Type,
+        target: &Type,     /*MappedType*/
+        constraint: &Type, /*IndexType*/
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_unmatched_properties(
+        &self,
+        source: &Type,
+        target: &Type,
+        require_optional_properties: bool,
+        match_discriminant_properties: bool,
+    ) -> Vec<Rc<Symbol>> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_unmatched_property(
+        &self,
+        source: &Type,
+        target: &Type,
+        require_optional_properties: bool,
+        match_discriminant_properties: bool,
+    ) -> Option<Rc<Symbol>> {
+        unimplemented!()
+    }
+
+    pub(super) fn template_literal_types_definitely_unrelated(
+        &self,
+        source: &Type, /*TemplateLiteralType*/
+        target: &Type, /*TemplateLiteralType*/
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_type_matched_by_template_literal_type(
+        &self,
+        source: &Type,
+        target: &Type, /*TemplateLiteralType*/
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn infer_types(
+        &self,
+        inferences: &[Rc<InferenceInfo>],
+        original_source: &Type,
+        original_target: &Type,
+        priority: Option<InferencePriority>,
+        contravariant: Option<bool>,
+    ) {
+        let priority = priority.unwrap_or(InferencePriority::None);
+        let contravariant = contravariant.unwrap_or(false);
+        unimplemented!()
+    }
+
+    pub(super) fn is_object_literal_type(&self, type_: &Type) -> bool {
+        get_object_flags(type_).intersects(ObjectFlags::ObjectLiteral)
+    }
+
+    pub(super) fn is_object_or_array_literal_type(&self, type_: &Type) -> bool {
+        get_object_flags(type_).intersects(ObjectFlags::ObjectLiteral | ObjectFlags::ArrayLiteral)
+    }
+
+    pub(super) fn get_inferred_type(&self, context: &InferenceContext, index: usize) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_default_type_argument_type(&self, is_in_java_script_file: bool) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_cannot_find_name_diagnostic_for_name(
+        &self,
+        node: &Node,
+    ) -> &'static DiagnosticMessage {
+        match node.as_identifier().escaped_text {
+            _ => {
+                if false {
+                    unimplemented!()
+                } else {
+                    &Diagnostics::Cannot_find_name_0
+                }
+            }
+        }
+    }
+
+    pub(super) fn get_resolved_symbol(&self, node: &Node /*Identifier*/) -> Rc<Symbol> {
+        let links = self.get_node_links(node);
+        let mut links_ref = links.borrow_mut();
+        if links_ref.resolved_symbol.is_none() {
+            links_ref.resolved_symbol = Some(if !node_is_missing(Some(node)) {
+                self.resolve_name_(
+                    Some(node),
+                    &node.as_identifier().escaped_text,
+                    SymbolFlags::Value | SymbolFlags::ExportValue,
+                    Some(self.get_cannot_find_name_diagnostic_for_name(node)),
+                    Some(node.node_wrapper()),
+                    !is_write_only_access(node),
+                    Some(false),
+                )
+                .unwrap_or_else(|| self.unknown_symbol())
+            } else {
+                self.unknown_symbol()
+            });
+        }
+        links_ref.resolved_symbol.clone().unwrap()
+    }
+
+    pub(super) fn is_in_type_query(&self, node: &Node) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_matching_reference(&self, source: &Node, target: &Node) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn find_discriminant_properties(
+        &self,
+        source_properties: &[Rc<Symbol>],
+        target: &Type,
+    ) -> Option<Vec<Rc<Symbol>>> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_matching_union_constituent_for_type(
+        &self,
+        union_type: &Type, /*UnionType*/
+        type_: &Type,
+    ) -> Option<Rc<Type>> {
+        unimplemented!()
+    }
+
+    pub(super) fn has_matching_argument(
+        &self,
+        expression: &Node, /*CallExpression | NewExpression*/
+        reference: &Node,
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_function_object_type(&self, type_: &Type /*ObjectType*/) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_facts(&self, type_: &Type, ignore_objects: Option<bool>) -> TypeFacts {
+        let ignore_objects = ignore_objects.unwrap_or(false);
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_with_facts(&self, type_: &Type, include: TypeFacts) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_of_initializer(&self, node: &Node /*Expression*/) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn is_type_subset_of(&self, source: &Type, target: &Type) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn for_each_type<TReturn, TCallback: FnMut(&Type) -> Option<TReturn>>(
+        &self,
+        type_: &Type,
+        mut f: TCallback,
+    ) -> Option<TReturn> {
+        unimplemented!()
+    }
+
+    pub(super) fn some_type<TCallback: FnMut(&Type) -> bool>(
+        &self,
+        type_: &Type,
+        mut f: TCallback,
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn every_type<TCallback: FnMut(&Type) -> bool>(
+        &self,
+        type_: &Type,
+        mut f: TCallback,
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn filter_type<TCallback: FnMut(&Type) -> bool>(
+        &self,
+        type_: &Type,
+        mut f: TCallback,
+    ) -> Rc<Type> {
+        if type_.flags().intersects(TypeFlags::Union) {
+            unimplemented!()
+        }
+        if type_.flags().intersects(TypeFlags::Never) || f(type_) {
+            type_.type_wrapper()
+        } else {
+            self.never_type()
+        }
+    }
+
+    pub(super) fn remove_type(&self, type_: &Type, target_type: &Type) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn count_types(&self, type_: &Type) -> usize {
+        unimplemented!()
+    }
+
+    pub(super) fn map_type<TMapper: FnMut(&Type) -> Option<Rc<Type>>>(
+        &self,
+        type_: &Type,
+        mapper: &mut TMapper,
+        no_reductions: Option<bool>,
+    ) -> Option<Rc<Type>> {
+        let no_reductions = no_reductions.unwrap_or(false);
+        if type_.flags().intersects(TypeFlags::Never) {
+            return Some(type_.type_wrapper());
+        }
+        if !type_.flags().intersects(TypeFlags::Union) {
+            return mapper(type_);
+        }
+        let types = type_.as_union_or_intersection_type_interface().types();
+        let mut mapped_types: Vec<Rc<Type>> = vec![];
+        let mut changed = false;
+        for t in types {
+            let mapped = if t.flags().intersects(TypeFlags::Union) {
+                self.map_type(&t, mapper, Some(no_reductions))
+            } else {
+                mapper(&t)
+            };
+            changed = changed
+                || match mapped.as_ref() {
+                    None => true,
+                    Some(mapped) => !Rc::ptr_eq(t, mapped),
+                };
+            if let Some(mapped) = mapped {
+                mapped_types.push(mapped);
+            }
+        }
+        if changed {
+            if !mapped_types.is_empty() {
+                Some(self.get_union_type(
+                    mapped_types,
+                    Some(if no_reductions {
+                        UnionReduction::None
+                    } else {
+                        UnionReduction::Literal
+                    }),
+                    Option::<&Symbol>::None,
+                    None,
+                    Option::<&Type>::None,
+                ))
+            } else {
+                None
+            }
+        } else {
+            Some(type_.type_wrapper())
+        }
+    }
+
+    pub(super) fn map_type_with_alias<
+        TMapper: FnMut(&Type) -> Rc<Type>,
+        TAliasSymbol: Borrow<Symbol>,
+    >(
+        &self,
+        type_: &Type,
+        mapper: &mut TMapper,
+        alias_symbol: Option<TAliasSymbol>,
+        alias_type_arguments: Option<&[Rc<Type>]>,
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_constituent_count(&self, type_: &Type) -> usize {
+        if type_.flags().intersects(TypeFlags::Union) {
+            type_
+                .as_union_or_intersection_type_interface()
+                .types()
+                .len()
+        } else {
+            1
+        }
+    }
+
+    pub(super) fn extract_types_of_kind(&self, type_: &Type, kind: TypeFlags) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_flow_type_of_reference<
+        TInitialType: Borrow<Type>,
+        TFlowContainer: Borrow<Node>,
+    >(
+        &self,
+        reference: &Node,
+        declared_type: &Type,
+        initial_type: Option<TInitialType>,
+        flow_container: Option<TFlowContainer>,
+    ) -> Rc<Type> {
+        let initial_type = initial_type.map_or_else(
+            || declared_type.type_wrapper(),
+            |initial_type| initial_type.borrow().type_wrapper(),
+        );
+        unimplemented!()
+    }
+}
