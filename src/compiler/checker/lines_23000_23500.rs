a34e295955f6e0960ffb6692d7f22cbc39029e8c
Julian Rosse <julian@helixbass.net>
2022-08-12 16:54:04 -0400

    split file

--- src/compiler/checker/lines_23000_23500.rs
+++ src/compiler/checker/lines_23000_23500.rs
@@ -1147,25 +1147,4 @@ impl TypeChecker {
             }
         }
     }
-
-    pub(super) fn extract_types_of_kind(&self, type_: &Type, kind: TypeFlags) -> Rc<Type> {
-        self.filter_type(type_, |t: &Type| t.flags().intersects(kind))
-    }
-
-    pub(super) fn get_flow_type_of_reference<
-        TInitialType: Borrow<Type>,
-        TFlowContainer: Borrow<Node>,
-    >(
-        &self,
-        reference: &Node,
-        declared_type: &Type,
-        initial_type: Option<TInitialType>,
-        flow_container: Option<TFlowContainer>,
-    ) -> Rc<Type> {
-        let initial_type = initial_type.map_or_else(
-            || declared_type.type_wrapper(),
-            |initial_type| initial_type.borrow().type_wrapper(),
-        );
-        unimplemented!()
-    }
 }
