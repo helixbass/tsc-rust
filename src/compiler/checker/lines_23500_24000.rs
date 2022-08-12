1f73ed913ec96a60b89b774ace4a4b0aa919d43d
Julian Rosse <julian@helixbass.net>
2022-08-12 16:54:41 -0400

    rm unused

--- src/compiler/checker/lines_23500_24000.rs
+++ src/compiler/checker/lines_23500_24000.rs
@@ -1,29 +1,9 @@
 #![allow(non_upper_case_globals)]
 
 use std::borrow::Borrow;
-use std::convert::TryInto;
-use std::ptr;
 use std::rc::Rc;
 
-use super::IterationUse;
-use crate::{
-    add_related_info, contains_rc, create_diagnostic_for_node, create_file_diagnostic, every,
-    filter, find_ancestor, for_each, for_each_bool, get_check_flags,
-    get_effective_return_type_node, get_effective_type_annotation_node, get_object_flags,
-    get_source_file_of_node, get_span_of_token_at_position, get_symbol_name_for_private_identifier,
-    has_initializer, is_access_expression, is_assignment_target,
-    is_function_expression_or_arrow_function, is_function_or_module_block, is_identifier,
-    is_in_js_file, is_optional_chain, is_parameter, is_private_identifier,
-    is_property_access_expression, is_property_declaration, is_property_signature,
-    is_push_or_unshift_identifier, is_string_literal_like, is_variable_declaration, map,
-    skip_parentheses, some, CheckFlags, Diagnostic, Diagnostics, EvolvingArrayType, FlowFlags,
-    FlowNode, FlowNodeBase, FlowType, HasInitializerInterface, IncompleteType,
-    NamedDeclarationInterface, Node, NodeFlags, NodeInterface, ObjectFlags,
-    ObjectFlagsTypeInterface, ReadonlyTextRange, Signature, SignatureKind, Symbol, SymbolFlags,
-    SymbolInterface, SyntaxKind, TransientSymbolInterface, Type, TypeChecker, TypeFlags,
-    TypeInterface, TypePredicate, TypePredicateKind, UnionOrIntersectionTypeInterface,
-    UnionReduction,
-};
+use crate::{Node, SymbolInterface, Type, TypeChecker, TypeFlags, TypeInterface};
 
 impl TypeChecker {
     pub(super) fn extract_types_of_kind(&self, type_: &Type, kind: TypeFlags) -> Rc<Type> {
