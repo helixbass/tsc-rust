df41aa63f752c2dcbb3ceb98ad4231303144e14f
Julian Rosse <julian@helixbass.net>
2022-08-14 14:14:47 -0400

    narrow type by type predicate

--- src/compiler/checker/lines_24500_25000.rs
+++ src/compiler/checker/lines_24500_25000.rs
@@ -4,9 +4,11 @@ use std::rc::Rc;
 
 use super::{GetFlowTypeOfReference, TypeFacts};
 use crate::{
-    are_option_rcs_equal, find, map, Node, Signature, SignatureKind, Symbol, SyntaxKind, Type,
-    TypePredicate, UnionOrIntersectionTypeInterface, __String, get_object_flags, ObjectFlags,
-    TypeFlags, TypeInterface,
+    are_option_rcs_equal, escape_leading_underscores, find, is_access_expression, is_call_chain,
+    is_identifier, is_property_access_expression, is_string_literal_like, map, Node, Signature,
+    SignatureKind, Symbol, SyntaxKind, Type, TypePredicate, TypePredicateKind,
+    UnionOrIntersectionTypeInterface, __String, get_object_flags, ObjectFlags, TypeFlags,
+    TypeInterface,
 };
 
 impl GetFlowTypeOfReference {
@@ -201,9 +203,109 @@ impl GetFlowTypeOfReference {
         type_: &Type,
         candidate: &Type,
         assume_true: bool,
-        is_related: TIsRelated,
+        mut is_related: TIsRelated,
     ) -> Rc<Type> {
-        unimplemented!()
+        if !assume_true {
+            return self
+                .type_checker
+                .filter_type(type_, |t: &Type| !is_related(t, candidate));
+        }
+        if type_.flags().intersects(TypeFlags::Union) {
+            let assignable_type = self
+                .type_checker
+                .filter_type(type_, |t: &Type| is_related(t, candidate));
+            if !assignable_type.flags().intersects(TypeFlags::Never) {
+                return assignable_type;
+            }
+        }
+
+        if self.type_checker.is_type_subtype_of(candidate, type_) {
+            candidate.type_wrapper()
+        } else if self.type_checker.is_type_assignable_to(type_, candidate) {
+            type_.type_wrapper()
+        } else if self.type_checker.is_type_assignable_to(candidate, type_) {
+            candidate.type_wrapper()
+        } else {
+            self.type_checker.get_intersection_type(
+                &vec![type_.type_wrapper(), candidate.type_wrapper()],
+                Option::<&Symbol>::None,
+                None,
+            )
+        }
+    }
+
+    pub(super) fn narrow_type_by_call_expression(
+        &self,
+        type_: &Type,
+        call_expression: &Node, /*CallExpression*/
+        assume_true: bool,
+    ) -> Rc<Type> {
+        if self
+            .type_checker
+            .has_matching_argument(call_expression, &self.reference)
+        {
+            let signature = if assume_true || !is_call_chain(call_expression) {
+                self.type_checker.get_effects_signature(call_expression)
+            } else {
+                None
+            };
+            let predicate = signature
+                .as_ref()
+                .and_then(|signature| self.type_checker.get_type_predicate_of_signature(signature));
+            if let Some(predicate) = predicate.as_ref().filter(|predicate| {
+                matches!(
+                    predicate.kind,
+                    TypePredicateKind::This | TypePredicateKind::Identifier
+                )
+            }) {
+                return self.narrow_type_by_type_predicate(
+                    type_,
+                    predicate,
+                    call_expression,
+                    assume_true,
+                );
+            }
+        }
+        let call_expression_as_call_expression = call_expression.as_call_expression();
+        if self.type_checker.contains_missing_type(type_)
+            && is_access_expression(&self.reference)
+            && is_property_access_expression(&call_expression_as_call_expression.expression)
+        {
+            let call_access = &call_expression_as_call_expression.expression;
+            let call_access_as_property_access_expression =
+                call_access.as_property_access_expression();
+            if self.type_checker.is_matching_reference(
+                &self.reference.as_has_expression().expression(),
+                &self
+                    .type_checker
+                    .get_reference_candidate(&call_access_as_property_access_expression.expression),
+            ) && is_identifier(&call_access_as_property_access_expression.name)
+                && call_access_as_property_access_expression
+                    .name
+                    .as_identifier()
+                    .escaped_text
+                    .eq_str("hasOwnProperty")
+                && call_expression_as_call_expression.arguments.len() == 1
+            {
+                let argument = &call_expression_as_call_expression.arguments[0];
+                if is_string_literal_like(argument)
+                    && matches!(
+                        self.type_checker.get_accessed_property_name(&self.reference).as_ref(),
+                        Some(accessed_property_name) if accessed_property_name == &escape_leading_underscores(&argument.as_literal_like_node().text())
+                    )
+                {
+                    return self.type_checker.get_type_with_facts(
+                        type_,
+                        if assume_true {
+                            TypeFacts::NEUndefined
+                        } else {
+                            TypeFacts::EQUndefined
+                        },
+                    );
+                }
+            }
+        }
+        type_.type_wrapper()
     }
 
     pub(super) fn narrow_type_by_type_predicate(
@@ -213,7 +315,59 @@ impl GetFlowTypeOfReference {
         call_expression: &Node, /*CallExpression*/
         assume_true: bool,
     ) -> Rc<Type> {
-        unimplemented!()
+        let mut type_ = type_.type_wrapper();
+        if let Some(predicate_type) = predicate.type_.as_ref().filter(|predicate_type| {
+            !(self.type_checker.is_type_any(Some(&*type_))
+                && (Rc::ptr_eq(predicate_type, &self.type_checker.global_object_type())
+                    || Rc::ptr_eq(predicate_type, &self.type_checker.global_function_type())))
+        }) {
+            let predicate_argument = self
+                .type_checker
+                .get_type_predicate_argument(predicate, call_expression);
+            if let Some(predicate_argument) = predicate_argument.as_ref() {
+                if self
+                    .type_checker
+                    .is_matching_reference(&self.reference, predicate_argument)
+                {
+                    return self.get_narrowed_type(
+                        &type_,
+                        predicate_type,
+                        assume_true,
+                        |type1: &Type, type2: &Type| {
+                            self.type_checker.is_type_subtype_of(type1, type2)
+                        },
+                    );
+                }
+                if self.type_checker.strict_null_checks
+                    && assume_true
+                    && self
+                        .type_checker
+                        .optional_chain_contains_reference(predicate_argument, &self.reference)
+                    && !self
+                        .type_checker
+                        .get_type_facts(predicate_type, None)
+                        .intersects(TypeFacts::EQUndefined)
+                {
+                    type_ = self
+                        .type_checker
+                        .get_type_with_facts(&type_, TypeFacts::NEUndefinedOrNull);
+                }
+                let access = self.get_discriminant_property_access(predicate_argument, &type_);
+                if let Some(access) = access.as_ref() {
+                    return self.narrow_type_by_discriminant(&type_, access, |t: &Type| {
+                        self.get_narrowed_type(
+                            t,
+                            predicate_type,
+                            assume_true,
+                            |type1: &Type, type2: &Type| {
+                                self.type_checker.is_type_subtype_of(type1, type2)
+                            },
+                        )
+                    });
+                }
+            }
+        }
+        type_.type_wrapper()
     }
 
     pub(super) fn narrow_type(
