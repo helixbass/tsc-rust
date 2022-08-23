bfa5e5ae3f1745fe8189cb9ba5c2d205cc0d603c
Julian Rosse <julian@helixbass.net>
2022-08-23 15:46:41 -0400

    split file

---
+++ src/compiler/checker/lines_31000_32000.rs
@@ -0,0 +1,402 @@
+#![allow(non_upper_case_globals)]
+
+use std::borrow::Borrow;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::ptr;
+use std::rc::Rc;
+
+use super::{
+    signature_has_rest_parameter, CheckMode, GetDiagnosticSpanForCallNodeReturn,
+    InvocationErrorDetails, JsxNames, MinArgumentCountFlags, TypeFacts, WideningKind,
+};
+use crate::{
+    add_related_info, chain_diagnostic_messages, create_diagnostic_for_node,
+    create_diagnostic_for_node_array, create_diagnostic_for_node_from_message_chain,
+    create_symbol_table, every, factory, get_expando_initializer,
+    get_initializer_of_binary_expression, get_invoked_expression, get_jsdoc_class_tag,
+    get_source_file_of_node, get_symbol_id, get_text_of_node, is_array_literal_expression,
+    is_binary_expression, is_bindable_static_name_expression, is_call_expression, is_dotted_name,
+    is_function_declaration, is_function_expression, is_function_like_declaration, is_import_call,
+    is_in_js_file, is_jsdoc_construct_signature, is_object_literal_expression, is_prototype_access,
+    is_qualified_name, is_same_entity_name, is_transient_symbol, is_var_const,
+    is_variable_declaration, length, maybe_for_each, skip_parentheses, synthetic_factory,
+    try_get_property_access_or_identifier_to_string, walk_up_parenthesized_expressions, Debug_,
+    Diagnostic, DiagnosticMessage, DiagnosticRelatedInformation,
+    DiagnosticRelatedInformationInterface, Diagnostics, FunctionFlags, HasInitializerInterface,
+    NamedDeclarationInterface, NodeArray, NodeFlags, ObjectFlags, Signature, SignatureFlags,
+    SignatureKind, SymbolFlags, TransientSymbolInterface, UnionReduction, __String,
+    get_function_flags, has_initializer, Node, NodeInterface, Symbol, SymbolInterface, SyntaxKind,
+    Type, TypeChecker, TypeFlags, TypeInterface,
+};
+
+impl TypeChecker {
+    pub(super) fn is_symbol_or_symbol_for_call(&self, node: &Node) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_with_synthetic_default_only(
+        &self,
+        type_: &Type,
+        symbol: &Symbol,
+        original_symbol: &Symbol,
+        module_specifier: &Node, /*Expression*/
+    ) -> Option<Rc<Type>> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_with_synthetic_default_import_type(
+        &self,
+        type_: &Type,
+        symbol: &Symbol,
+        original_symbol: &Symbol,
+        module_specifier: &Node, /*Expression*/
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn is_common_js_require(&self, node: &Node) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_of_parameter(&self, symbol: &Symbol) -> Rc<Type> {
+        let type_ = self.get_type_of_symbol(symbol);
+        if self.strict_null_checks {
+            let declaration = symbol.maybe_value_declaration();
+            if matches!(declaration.as_ref(), Some(declaration) if has_initializer(&declaration)) {
+                return self.get_optional_type_(&type_, None);
+            }
+        }
+        type_
+    }
+
+    pub(super) fn get_tuple_element_label(
+        &self,
+        d: &Node, /*ParameterDeclaration | NamedTupleMember*/
+    ) -> __String {
+        unimplemented!()
+    }
+
+    pub(super) fn get_parameter_name_at_position<TOverrideRestType: Borrow<Type>>(
+        &self,
+        signature: &Signature,
+        pos: usize,
+        override_rest_type: Option<TOverrideRestType>,
+    ) -> __String {
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_at_position(&self, signature: &Signature, pos: usize) -> Rc<Type> {
+        self.try_get_type_at_position(signature, pos)
+            .unwrap_or_else(|| self.any_type())
+    }
+
+    pub(super) fn try_get_type_at_position(
+        &self,
+        signature: &Signature,
+        pos: usize,
+    ) -> Option<Rc<Type>> {
+        let param_count = signature.parameters().len()
+            - if signature_has_rest_parameter(signature) {
+                1
+            } else {
+                0
+            };
+        if pos < param_count {
+            return Some(self.get_type_of_parameter(&signature.parameters()[pos]));
+        }
+        if signature_has_rest_parameter(signature) {
+            let rest_type = self.get_type_of_symbol(&signature.parameters()[param_count]);
+            let index = pos - param_count;
+            unimplemented!()
+        }
+        None
+    }
+
+    pub(super) fn get_rest_type_at_position(&self, signature: &Signature, pos: usize) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_parameter_count(&self, signature: &Signature) -> usize {
+        let length = signature.parameters().len();
+        if signature_has_rest_parameter(signature) {
+            let rest_type = self.get_type_of_symbol(&signature.parameters()[length - 1]);
+            if self.is_tuple_type(&rest_type) {
+                unimplemented!()
+            }
+        }
+        length
+    }
+
+    pub(super) fn get_min_argument_count(
+        &self,
+        signature: &Signature,
+        flags: Option<MinArgumentCountFlags>,
+    ) -> usize {
+        let strong_arity_for_untyped_js = match flags {
+            None => false,
+            Some(flags) => flags.intersects(MinArgumentCountFlags::StrongArityForUntypedJS),
+        };
+        let void_is_non_optional = match flags {
+            None => false,
+            Some(flags) => flags.intersects(MinArgumentCountFlags::VoidIsNonOptional),
+        };
+        if void_is_non_optional || signature.maybe_resolved_min_argument_count().is_none() {
+            let mut min_argument_count = None;
+            if signature_has_rest_parameter(signature) {
+                let rest_type = self
+                    .get_type_of_symbol(&signature.parameters()[signature.parameters().len() - 1]);
+                if self.is_tuple_type(&rest_type) {
+                    unimplemented!()
+                }
+            }
+            if min_argument_count.is_none() {
+                if !strong_arity_for_untyped_js
+                    && signature
+                        .flags
+                        .intersects(SignatureFlags::IsUntypedSignatureInJSFile)
+                {
+                    return 0;
+                }
+                min_argument_count = Some(signature.min_argument_count());
+            }
+            let mut min_argument_count = min_argument_count.unwrap();
+            if void_is_non_optional {
+                return min_argument_count;
+            }
+            let mut i = min_argument_count - 1;
+            while i >= 0 {
+                let type_ = self.get_type_at_position(signature, i);
+                if self
+                    .filter_type(&type_, |type_| self.accepts_void(type_))
+                    .flags()
+                    .intersects(TypeFlags::Never)
+                {
+                    break;
+                }
+                min_argument_count = i;
+                i -= 1;
+            }
+            signature.set_resolved_min_argument_count(min_argument_count);
+        }
+        signature.resolved_min_argument_count()
+    }
+
+    pub(super) fn has_effective_rest_parameter(&self, signature: &Signature) -> bool {
+        if signature_has_rest_parameter(signature) {
+            let rest_type =
+                self.get_type_of_symbol(&signature.parameters()[signature.parameters().len() - 1]);
+            return !self.is_tuple_type(&rest_type) || unimplemented!();
+        }
+        false
+    }
+
+    pub(super) fn get_effective_rest_type(&self, signature: &Signature) -> Option<Rc<Type>> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_non_array_rest_type(&self, signature: &Signature) -> Option<Rc<Type>> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_of_first_parameter_of_signature(
+        &self,
+        signature: &Signature,
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_type_of_first_parameter_of_signature_with_fallback(
+        &self,
+        signature: &Signature,
+        fallback_type: &Type,
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn create_promise_type(&self, promised_type: &Type) -> Rc<Type> {
+        let global_promise_type = self.get_global_promise_type(true);
+        if !Rc::ptr_eq(&global_promise_type, &self.empty_generic_type()) {
+            let promised_type = self
+                .get_awaited_type_no_alias(
+                    &self.unwrap_awaited_type(promised_type),
+                    Option::<&Node>::None,
+                    None,
+                    None,
+                )
+                .unwrap_or_else(|| self.unknown_type());
+            return self.create_type_reference(&global_promise_type, Some(vec![promised_type]));
+        }
+
+        self.unknown_type()
+    }
+
+    pub(super) fn create_promise_like_type(&self, promised_type: &Type) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn create_promise_return_type(
+        &self,
+        func: &Node, /*FunctionLikeDeclaration | ImportCall*/
+        promised_type: &Type,
+    ) -> Rc<Type> {
+        let promise_type = self.create_promise_type(promised_type);
+        if Rc::ptr_eq(&promise_type, &self.unknown_type()) {
+            self.error(
+                Some(func),
+                if is_import_call(func) {
+                    &Diagnostics::A_dynamic_import_call_returns_a_Promise_Make_sure_you_have_a_declaration_for_Promise_or_include_ES2015_in_your_lib_option
+                } else {
+                    &Diagnostics::An_async_function_or_method_must_return_a_Promise_Make_sure_you_have_a_declaration_for_Promise_or_include_ES2015_in_your_lib_option
+                },
+                None
+            );
+            return self.error_type();
+        } else if self.get_global_promise_constructor_symbol(true).is_none() {
+            self.error(
+                Some(func),
+                if is_import_call(func) {
+                    &Diagnostics::A_dynamic_import_call_in_ES5_SlashES3_requires_the_Promise_constructor_Make_sure_you_have_a_declaration_for_the_Promise_constructor_or_include_ES2015_in_your_lib_option
+                } else {
+                    &Diagnostics::An_async_function_or_method_in_ES5_SlashES3_requires_the_Promise_constructor_Make_sure_you_have_a_declaration_for_the_Promise_constructor_or_include_ES2015_in_your_lib_option
+                },
+                None
+            );
+        }
+
+        promise_type
+    }
+
+    pub(super) fn get_return_type_from_body(
+        &self,
+        func: &Node, /*FunctionLikeDeclaration*/
+        check_mode: Option<CheckMode>,
+    ) -> Rc<Type> {
+        let func_as_function_like_declaration = func.as_function_like_declaration();
+        if func_as_function_like_declaration.maybe_body().is_none() {
+            return self.error_type();
+        }
+        let func_body = func_as_function_like_declaration.maybe_body().unwrap();
+
+        let function_flags = get_function_flags(Some(func));
+        let is_async = function_flags.intersects(FunctionFlags::Async);
+        let is_generator = function_flags.intersects(FunctionFlags::Generator);
+
+        let mut return_type: Option<Rc<Type>> = None;
+        let mut yield_type: Option<Rc<Type>> = None;
+        let mut next_type: Option<Rc<Type>> = None;
+        let fallback_return_type = self.void_type();
+        if func_body.kind() != SyntaxKind::Block {
+            return_type = Some(self.check_expression_cached(
+                &func_body,
+                check_mode.map(|check_mode| check_mode & !CheckMode::SkipGenericFunctions),
+            ));
+            if is_async {
+                unimplemented!()
+            }
+        } else if is_generator {
+            unimplemented!()
+        } else {
+            let types = self.check_and_aggregate_return_expression_types(func, check_mode);
+            if types.is_none() {
+                return if function_flags.intersects(FunctionFlags::Async) {
+                    self.create_promise_return_type(func, &self.never_type())
+                } else {
+                    self.never_type()
+                };
+            }
+            let types = types.unwrap();
+            if types.is_empty() {
+                return if function_flags.intersects(FunctionFlags::Async) {
+                    self.create_promise_return_type(func, &self.void_type())
+                } else {
+                    self.void_type()
+                };
+            }
+
+            return_type = Some(self.get_union_type(
+                types,
+                Some(UnionReduction::Subtype),
+                Option::<&Symbol>::None,
+                None,
+                Option::<&Type>::None,
+            ));
+        }
+
+        if return_type.is_some() || yield_type.is_some() || next_type.is_some() {
+            if let Some(yield_type) = yield_type.as_ref() {
+                self.report_errors_from_widening(
+                    func,
+                    yield_type,
+                    Some(WideningKind::GeneratorYield),
+                );
+            }
+            if let Some(return_type) = return_type.as_ref() {
+                self.report_errors_from_widening(
+                    func,
+                    return_type,
+                    Some(WideningKind::FunctionReturn),
+                );
+            }
+            if let Some(next_type) = next_type.as_ref() {
+                self.report_errors_from_widening(
+                    func,
+                    next_type,
+                    Some(WideningKind::GeneratorNext),
+                );
+            }
+
+            if matches!(return_type.as_ref(), Some(return_type) if self.is_unit_type(return_type))
+                || matches!(yield_type.as_ref(), Some(yield_type) if self.is_unit_type(yield_type))
+                || matches!(next_type.as_ref(), Some(next_type) if self.is_unit_type(next_type))
+            {
+                unimplemented!()
+            }
+
+            if let Some(yield_type_present) = yield_type {
+                yield_type = Some(self.get_widened_type(&yield_type_present));
+            }
+            if let Some(return_type_present) = return_type {
+                return_type = Some(self.get_widened_type(&return_type_present));
+            }
+            if let Some(next_type_present) = next_type {
+                next_type = Some(self.get_widened_type(&next_type_present));
+            }
+        }
+
+        if is_generator {
+            unimplemented!()
+        } else {
+            if is_async {
+                self.create_promise_type(&return_type.unwrap_or(fallback_return_type))
+            } else {
+                return_type.unwrap_or(fallback_return_type)
+            }
+        }
+    }
+
+    pub(super) fn get_facts_from_typeof_switch(
+        &self,
+        start: usize,
+        end: usize,
+        witnesses: &[String],
+        has_default: bool,
+    ) -> TypeFacts {
+        unimplemented!()
+    }
+
+    pub(super) fn is_exhaustive_switch_statement(
+        &self,
+        node: &Node, /*SwitchStatement*/
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn check_and_aggregate_return_expression_types(
+        &self,
+        func: &Node, /*FunctionLikeDeclaration*/
+        check_mode: Option<CheckMode>,
+    ) -> Option<Vec<Rc<Type>>> {
+        unimplemented!()
+    }
+}
