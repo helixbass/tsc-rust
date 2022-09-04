9902d14fe68b00e16eb11b0f8fb26c9b58a8de55
Julian Rosse <julian@helixbass.net>
2022-09-03 20:18:22 -0400

    split file

---
+++ src/compiler/checker/lines_37000_40000.rs
@@ -0,0 +1,289 @@
+#![allow(non_upper_case_globals)]
+
+use std::borrow::Borrow;
+use std::ptr;
+use std::rc::Rc;
+
+use super::{CheckMode, IterationTypeKind, IterationUse};
+use crate::{
+    add_related_info, are_option_rcs_equal, create_diagnostic_for_node, declaration_name_to_string,
+    find_ancestor, for_each, for_each_child_bool, get_ancestor, get_combined_node_flags,
+    get_containing_function, get_containing_function_or_class_static_block,
+    get_effective_initializer, get_enclosing_block_scope_container, get_function_flags,
+    get_module_instance_state, get_name_of_declaration, get_selected_effective_modifier_flags,
+    get_source_file_of_node, has_question_token, is_array_binding_pattern, is_binary_expression,
+    is_binding_element, is_binding_pattern, is_call_expression, is_class_expression, is_class_like,
+    is_enum_declaration, is_external_or_common_js_module, is_function_expression, is_function_like,
+    is_identifier, is_in_js_file, is_module_declaration, is_named_declaration,
+    is_object_binding_pattern, is_object_literal_expression, is_parameter_declaration,
+    is_property_access_expression, is_prototype_access, is_require_variable_declaration,
+    is_variable_like, node_is_missing, some, ClassLikeDeclarationInterface, Debug_,
+    DiagnosticMessage, Diagnostics, ExternalEmitHelpers, FunctionFlags, HasInitializerInterface,
+    HasTypeParametersInterface, IterationTypes, IterationTypesResolver, ModifierFlags,
+    ModuleInstanceState, ModuleKind, Node, NodeArray, NodeCheckFlags, NodeFlags, NodeInterface,
+    ScriptTarget, SignatureKind, Symbol, SymbolFlags, SymbolInterface, SyntaxKind, Type,
+    TypeChecker, TypeFlags, TypeInterface,
+};
+
+impl TypeChecker {
+    pub(super) fn is_symbol_used_in_binary_expression_chain(
+        &self,
+        node: &Node,
+        tested_symbol: &Symbol,
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn check_truthiness_of_type(&self, type_: &Type, node: &Node) -> Rc<Type> {
+        if type_.flags().intersects(TypeFlags::Void) {
+            self.error(
+                Some(node),
+                &Diagnostics::An_expression_of_type_void_cannot_be_tested_for_truthiness,
+                None,
+            );
+        }
+
+        type_.type_wrapper()
+    }
+
+    pub(super) fn check_truthiness_expression(
+        &self,
+        node: &Node, /*Expression*/
+        check_mode: Option<CheckMode>,
+    ) -> Rc<Type> {
+        self.check_truthiness_of_type(&self.check_expression(node, check_mode, None), node)
+    }
+
+    pub(super) fn check_right_hand_side_of_for_of(
+        &self,
+        statement: &Node, /*ForOfStatement*/
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn check_iterated_type_or_element_type<TErrorNode: Borrow<Node>>(
+        &self,
+        use_: IterationUse,
+        input_type: &Type,
+        sent_type: &Type,
+        error_node: Option<TErrorNode>,
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_iterated_type_or_element_type<TErrorNode: Borrow<Node>>(
+        &self,
+        use_: IterationUse,
+        input_type: &Type,
+        sent_type: &Type,
+        error_node: Option<TErrorNode>,
+        check_assignability: bool,
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_iteration_type_of_iterable<TErrorNode: Borrow<Node>>(
+        &self,
+        use_: IterationUse,
+        type_kind: IterationTypeKind,
+        input_type: &Type,
+        error_node: Option<TErrorNode>,
+    ) -> Option<Rc<Type>> {
+        unimplemented!()
+    }
+
+    pub(super) fn create_iteration_types(
+        &self,
+        yield_type: Option<Rc<Type>>,
+        return_type: Option<Rc<Type>>,
+        next_type: Option<Rc<Type>>,
+    ) -> IterationTypes {
+        let yield_type = yield_type.unwrap_or_else(|| self.never_type());
+        let return_type = return_type.unwrap_or_else(|| self.never_type());
+        let next_type = next_type.unwrap_or_else(|| self.unknown_type());
+        IterationTypes::new(yield_type, return_type, next_type)
+    }
+
+    pub(super) fn get_iteration_types_of_iterable<TErrorNode: Borrow<Node>>(
+        &self,
+        type_: &Type,
+        use_: IterationUse,
+        error_node: Option<TErrorNode>,
+    ) -> Option<IterationTypes> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_iteration_types_of_global_iterable_type(
+        &self,
+        global_type: &Type,
+        resolver: &IterationTypesResolver,
+    ) -> IterationTypes {
+        unimplemented!()
+    }
+
+    pub(super) fn get_iteration_type_of_generator_function_return_type(
+        &self,
+        kind: IterationTypeKind,
+        return_type: &Type,
+        is_async_generator: bool,
+    ) -> Option<Rc<Type>> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_iteration_types_of_generator_function_return_type(
+        &self,
+        type_: &Type,
+        is_async_generator: bool,
+    ) -> Option<IterationTypes> {
+        unimplemented!()
+    }
+
+    pub(super) fn unwrap_return_type(
+        &self,
+        return_type: &Type,
+        function_flags: FunctionFlags,
+    ) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn is_unwrapped_return_type_void_or_any(
+        &self,
+        func: &Node, /*SignatureDeclaration*/
+        return_type: &Type,
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn check_return_statement(&self, node: &Node /*ReturnStatement*/) {
+        let container = get_containing_function_or_class_static_block(node);
+
+        if container.is_none() {
+            unimplemented!()
+        }
+        let container = container.unwrap();
+
+        let signature = self.get_signature_from_declaration_(&container);
+        let return_type = self.get_return_type_of_signature(signature);
+        let function_flags = get_function_flags(Some(&*container));
+        let node_as_return_statement = node.as_return_statement();
+        if self.strict_null_checks
+            || node_as_return_statement.expression.is_some()
+            || return_type.flags().intersects(TypeFlags::Never)
+        {
+            let expr_type = match node_as_return_statement.expression.as_ref() {
+                Some(expression) => self.check_expression_cached(&expression, None),
+                None => self.undefined_type(),
+            };
+            if false {
+                unimplemented!()
+            } else if self.get_return_type_from_annotation(&container).is_some() {
+                let unwrapped_return_type = self
+                    .unwrap_return_type(&return_type, function_flags)/*.unwrap_or(return_type)*/;
+                let unwrapped_expr_type = if function_flags.intersects(FunctionFlags::Async) {
+                    self.check_awaited_type(&expr_type, false, node, &Diagnostics::The_return_type_of_an_async_function_must_either_be_a_valid_promise_or_must_not_contain_a_callable_then_member, None)
+                } else {
+                    expr_type
+                };
+                // if unwrappedReturnType {
+                self.check_type_assignable_to_and_optionally_elaborate(
+                    &unwrapped_expr_type,
+                    &unwrapped_return_type,
+                    Some(node),
+                    node_as_return_statement.expression.clone(),
+                    None,
+                    None,
+                );
+                // }
+            }
+        }
+    }
+
+    pub(super) fn check_index_constraints(
+        &self,
+        type_: &Type,
+        symbol: &Symbol,
+        is_static_index: Option<bool>,
+    ) {
+        unimplemented!()
+    }
+
+    pub(super) fn check_type_name_is_reserved(
+        &self,
+        name: &Node, /*Identifier*/
+        message: &'static DiagnosticMessage,
+    ) {
+        unimplemented!()
+    }
+
+    pub(super) fn check_class_name_collision_with_object(&self, name: &Node /*Identifier*/) {
+        unimplemented!()
+    }
+
+    pub(super) fn check_type_parameters(
+        &self,
+        type_parameter_declarations: Option<&[Rc<Node /*TypeParameterDeclaration*/>]>,
+    ) {
+        if let Some(type_parameter_declarations) = type_parameter_declarations {
+            for node in type_parameter_declarations {
+                self.check_type_parameter(&node);
+            }
+        }
+    }
+
+    pub(super) fn check_class_expression(&self, node: &Node /*ClassExpression*/) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_target_symbol(&self, s: &Symbol) -> Rc<Symbol> {
+        unimplemented!()
+    }
+
+    pub(super) fn is_property_without_initializer(&self, node: &Node) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_property_initialized_in_static_blocks(
+        &self,
+        prop_name: &Node, /*Identifier | PrivateIdentifier*/
+        prop_type: &Type,
+        static_blocks: &[Rc<Node /*ClassStaticBlockDeclaration*/>],
+        start_pos: isize,
+        end_pos: isize,
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn check_interface_declaration(&self, node: &Node /*InterfaceDeclaration*/) {
+        let node_as_interface_declaration = node.as_interface_declaration();
+        self.check_type_parameters(
+            node_as_interface_declaration
+                .maybe_type_parameters()
+                .as_deref(),
+        );
+        for_each(&node_as_interface_declaration.members, |member, _| {
+            self.check_source_element(Some(&**member));
+            Option::<()>::None
+        });
+    }
+
+    pub(super) fn check_type_alias_declaration(&self, node: &Node /*TypeAliasDeclaration*/) {
+        let node_as_type_alias_declaration = node.as_type_alias_declaration();
+        self.check_type_parameters(
+            node_as_type_alias_declaration
+                .maybe_type_parameters()
+                .as_deref(),
+        );
+        if false {
+            unimplemented!()
+        } else {
+            self.check_source_element(Some(&*node_as_type_alias_declaration.type_));
+        }
+    }
+
+    pub(super) fn check_alias_symbol(
+        &self,
+        node: &Node, /*ImportEqualsDeclaration | VariableDeclaration | ImportClause | NamespaceImport | ImportSpecifier | ExportSpecifier | NamespaceExport*/
+    ) {
+        unimplemented!()
+    }
+}
