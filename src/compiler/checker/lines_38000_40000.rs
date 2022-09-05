ac19d97c1c536f2e209a621e877bdda1feef8b30
Julian Rosse <julian@helixbass.net>
2022-09-05 10:41:37 -0400

    split file

---
+++ src/compiler/checker/lines_38000_40000.rs
@@ -0,0 +1,108 @@
+#![allow(non_upper_case_globals)]
+
+use std::borrow::Borrow;
+use std::ptr;
+use std::rc::Rc;
+
+use super::{
+    get_iteration_types_key_from_iteration_type_kind, IterationTypeKind, IterationUse, TypeFacts,
+};
+use crate::{
+    append, is_class_static_block_declaration, map, some, ObjectTypeInterface, Signature,
+    SignatureKind, SymbolFlags, SymbolInterface, SyntaxKind, __String, escape_leading_underscores,
+    for_each, get_containing_function_or_class_static_block, get_function_flags, DiagnosticMessage,
+    Diagnostics, FunctionFlags, HasTypeParametersInterface, IterationTypeCacheKey, IterationTypes,
+    IterationTypesResolver, Node, NodeInterface, Symbol, Type, TypeChecker, TypeFlags,
+    TypeInterface,
+};
+
+impl TypeChecker {
+    pub(super) fn check_index_constraints(
+        &self,
+        type_: &Type,
+        symbol: &Symbol,
+        is_static_index: Option<bool>,
+    ) {
+        unimplemented!()
+    }
+
+    pub(super) fn check_type_name_is_reserved(
+        &self,
+        name: &Node, /*Identifier*/
+        message: &'static DiagnosticMessage,
+    ) {
+        unimplemented!()
+    }
+
+    pub(super) fn check_class_name_collision_with_object(&self, name: &Node /*Identifier*/) {
+        unimplemented!()
+    }
+
+    pub(super) fn check_type_parameters(
+        &self,
+        type_parameter_declarations: Option<&[Rc<Node /*TypeParameterDeclaration*/>]>,
+    ) {
+        if let Some(type_parameter_declarations) = type_parameter_declarations {
+            for node in type_parameter_declarations {
+                self.check_type_parameter(&node);
+            }
+        }
+    }
+
+    pub(super) fn check_class_expression(&self, node: &Node /*ClassExpression*/) -> Rc<Type> {
+        unimplemented!()
+    }
+
+    pub(super) fn get_target_symbol(&self, s: &Symbol) -> Rc<Symbol> {
+        unimplemented!()
+    }
+
+    pub(super) fn is_property_without_initializer(&self, node: &Node) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn is_property_initialized_in_static_blocks(
+        &self,
+        prop_name: &Node, /*Identifier | PrivateIdentifier*/
+        prop_type: &Type,
+        static_blocks: &[Rc<Node /*ClassStaticBlockDeclaration*/>],
+        start_pos: isize,
+        end_pos: isize,
+    ) -> bool {
+        unimplemented!()
+    }
+
+    pub(super) fn check_interface_declaration(&self, node: &Node /*InterfaceDeclaration*/) {
+        let node_as_interface_declaration = node.as_interface_declaration();
+        self.check_type_parameters(
+            node_as_interface_declaration
+                .maybe_type_parameters()
+                .as_deref(),
+        );
+        for_each(&node_as_interface_declaration.members, |member, _| {
+            self.check_source_element(Some(&**member));
+            Option::<()>::None
+        });
+    }
+
+    pub(super) fn check_type_alias_declaration(&self, node: &Node /*TypeAliasDeclaration*/) {
+        let node_as_type_alias_declaration = node.as_type_alias_declaration();
+        self.check_type_parameters(
+            node_as_type_alias_declaration
+                .maybe_type_parameters()
+                .as_deref(),
+        );
+        if false {
+            unimplemented!()
+        } else {
+            self.check_source_element(Some(&*node_as_type_alias_declaration.type_));
+        }
+    }
+
+    pub(super) fn check_alias_symbol(
+        &self,
+        node: &Node, /*ImportEqualsDeclaration | VariableDeclaration | ImportClause | NamespaceImport | ImportSpecifier | ExportSpecifier | NamespaceExport*/
+    ) {
+        unimplemented!()
+    }
+}
