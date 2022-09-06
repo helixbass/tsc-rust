2b724b5352b16928bf1d1da374f3ebf7e787fb50
Julian Rosse <julian@helixbass.net>
2022-09-06 08:25:54 -0400

    get member override modifier status

--- src/compiler/checker/lines_38500_40000.rs
+++ src/compiler/checker/lines_38500_40000.rs
@@ -1,15 +1,20 @@
 #![allow(non_upper_case_globals)]
 
 use std::borrow::Borrow;
+use std::cell::RefCell;
 use std::rc::Rc;
 
+use super::CheckTypeContainingMessageChain;
 use crate::{
-    escape_leading_underscores, first, for_each, get_effective_base_type_node,
-    has_abstract_modifier, has_ambient_modifier, has_override_modifier, is_constructor_declaration,
-    is_in_js_file, is_parameter_property_declaration, is_static, some, symbol_name,
-    DiagnosticMessage, Diagnostics, HasTypeParametersInterface, InterfaceTypeInterface,
-    MemberOverrideStatus, Node, NodeFlags, NodeInterface, SignatureDeclarationInterface, Symbol,
-    SymbolInterface, Type, TypeChecker,
+    chain_diagnostic_messages, escape_leading_underscores, first, for_each,
+    get_class_like_declaration_of_symbol, get_effective_base_type_node, get_text_of_property_name,
+    has_abstract_modifier, has_ambient_modifier, has_effective_modifier, has_override_modifier,
+    has_syntactic_modifier, is_constructor_declaration, is_in_js_file,
+    is_parameter_property_declaration, is_static, some, symbol_name, unescape_leading_underscores,
+    DiagnosticMessage, DiagnosticMessageChain, Diagnostics, HasTypeParametersInterface,
+    InterfaceTypeInterface, MemberOverrideStatus, ModifierFlags, Node, NodeFlags, NodeInterface,
+    SignatureDeclarationInterface, SignatureKind, Symbol, SymbolInterface, Type, TypeChecker,
+    TypeInterface,
 };
 
 impl TypeChecker {
@@ -238,9 +243,64 @@ impl TypeChecker {
         node: &Node, /*ClassLikeDeclaration*/
         type_with_this: &Type,
         base_with_this: &Type,
-        broad_diag: &DiagnosticMessage,
+        broad_diag: &'static DiagnosticMessage,
     ) {
-        unimplemented!()
+        let mut issued_member_error = false;
+        let node_as_class_like_declaration = node.as_class_like_declaration();
+        for member in node_as_class_like_declaration.members() {
+            if is_static(member) {
+                continue;
+            }
+            let member_as_named_declaration = member.as_named_declaration();
+            let declared_prop = member_as_named_declaration
+                .maybe_name()
+                .as_ref()
+                .and_then(|member_name| self.get_symbol_at_location_(member_name, None))
+                .or_else(|| self.get_symbol_at_location_(member, None));
+            if let Some(declared_prop) = declared_prop.as_ref() {
+                let prop =
+                    self.get_property_of_type_(type_with_this, declared_prop.escaped_name(), None);
+                let base_prop =
+                    self.get_property_of_type_(base_with_this, declared_prop.escaped_name(), None);
+                if let (Some(prop), Some(base_prop)) = (prop.as_ref(), base_prop.as_ref()) {
+                    if !self.check_type_assignable_to(
+                        &self.get_type_of_symbol(prop),
+                        &self.get_type_of_symbol(base_prop),
+                        Some(
+                            member_as_named_declaration
+                                .maybe_name()
+                                .unwrap_or_else(|| member.clone()),
+                        ),
+                        None,
+                        Some(Rc::new(
+                            IssueMemberSpecificErrorContainingMessageChain::new(
+                                self.rc_wrapper(),
+                                declared_prop.clone(),
+                                type_with_this.type_wrapper(),
+                                base_with_this.type_wrapper(),
+                            ),
+                        )),
+                        None,
+                    ) {
+                        issued_member_error = true;
+                    }
+                }
+            }
+        }
+        if !issued_member_error {
+            self.check_type_assignable_to(
+                type_with_this,
+                base_with_this,
+                Some(
+                    node_as_class_like_declaration
+                        .maybe_name()
+                        .unwrap_or_else(|| node.node_wrapper()),
+                ),
+                Some(broad_diag),
+                None,
+                None,
+            );
+        }
     }
 
     pub(super) fn check_base_type_accessibility(
@@ -248,7 +308,87 @@ impl TypeChecker {
         type_: &Type,
         node: &Node, /*ExpressionWithTypeArguments*/
     ) {
-        unimplemented!()
+        let signatures = self.get_signatures_of_type(type_, SignatureKind::Construct);
+        if !signatures.is_empty() {
+            let declaration = signatures[0].declaration.as_ref();
+            if matches!(
+                declaration,
+                Some(declaration) if has_effective_modifier(declaration, ModifierFlags::Private)
+            ) {
+                let type_class_declaration =
+                    get_class_like_declaration_of_symbol(&type_.symbol()).unwrap();
+                if !self.is_node_within_class(node, &type_class_declaration) {
+                    self.error(
+                        Some(node),
+                        &Diagnostics::Cannot_extend_a_class_0_Class_constructor_is_marked_as_private,
+                        Some(vec![
+                            self.get_fully_qualified_name(
+                                &type_.symbol(),
+                                Option::<&Node>::None
+                            )
+                        ])
+                    );
+                }
+            }
+        }
+    }
+
+    pub(super) fn get_member_override_modifier_status(
+        &self,
+        node: &Node,   /*ClassLikeDeclaration*/
+        member: &Node, /*ClassElement*/
+    ) -> MemberOverrideStatus {
+        let member_name = member.as_named_declaration().maybe_name();
+        if member_name.is_none() {
+            return MemberOverrideStatus::Ok;
+        }
+        let member_name = member_name.unwrap();
+
+        let symbol = self.get_symbol_of_node(node).unwrap();
+        let type_ = self.get_declared_type_of_symbol(&symbol);
+        let type_with_this = self.get_type_with_this_argument(&type_, Option::<&Type>::None, None);
+        let static_type = self.get_type_of_symbol(&symbol);
+
+        let base_type_node = get_effective_base_type_node(node);
+        let base_types = if base_type_node.is_some() {
+            Some(self.get_base_types(&type_))
+        } else {
+            None
+        };
+        let base_with_this = base_types
+            .as_ref()
+            .filter(|base_types| !base_types.is_empty())
+            .map(|base_types| {
+                self.get_type_with_this_argument(
+                    &*first(base_types),
+                    type_.as_interface_type().maybe_this_type(),
+                    None,
+                )
+            });
+        let base_static_type = self.get_base_constructor_type_of_class(&type_);
+
+        let member_has_override_modifier = if member.maybe_parent().is_some() {
+            has_override_modifier(member)
+        } else {
+            has_syntactic_modifier(member, ModifierFlags::Override)
+        };
+
+        let member_name = unescape_leading_underscores(&get_text_of_property_name(&member_name));
+
+        self.check_member_for_override_modifier(
+            node,
+            &static_type,
+            &base_static_type,
+            base_with_this,
+            &type_,
+            &type_with_this,
+            member_has_override_modifier,
+            has_abstract_modifier(member),
+            is_static(member),
+            false,
+            &member_name,
+            Option::<&Node>::None,
+        )
     }
 
     pub(super) fn get_target_symbol(&self, s: &Symbol) -> Rc<Symbol> {
@@ -321,5 +461,56 @@ impl TypeChecker {
         node: &Node, /*ImportEqualsDeclaration | VariableDeclaration | ImportClause | NamespaceImport | ImportSpecifier | ExportSpecifier | NamespaceExport*/
     ) {
         unimplemented!()
+    }
+}
+
+struct IssueMemberSpecificErrorContainingMessageChain {
+    type_checker: Rc<TypeChecker>,
+    declared_prop: Rc<Symbol>,
+    type_with_this: Rc<Type>,
+    base_with_this: Rc<Type>,
+}
+
+impl IssueMemberSpecificErrorContainingMessageChain {
+    pub fn new(
+        type_checker: Rc<TypeChecker>,
+        declared_prop: Rc<Symbol>,
+        type_with_this: Rc<Type>,
+        base_with_this: Rc<Type>,
+    ) -> Self {
+        Self {
+            type_checker,
+            declared_prop,
+            type_with_this,
+            base_with_this,
+        }
+    }
+}
+
+impl CheckTypeContainingMessageChain for IssueMemberSpecificErrorContainingMessageChain {
+    fn get(&self) -> Option<Rc<RefCell<DiagnosticMessageChain>>> {
+        Some(Rc::new(RefCell::new(
+            chain_diagnostic_messages(
+                None,
+                &Diagnostics::Property_0_in_type_1_is_not_assignable_to_the_same_property_in_base_type_2,
+                Some(vec![
+                    self.type_checker.symbol_to_string_(
+                        &self.declared_prop,
+                        Option::<&Node>::None,
+                        None, None, None,
+                    ),
+                    self.type_checker.type_to_string_(
+                        &self.type_with_this,
+                        Option::<&Node>::None,
+                        None, None,
+                    ),
+                    self.type_checker.type_to_string_(
+                        &self.base_with_this,
+                        Option::<&Node>::None,
+                        None, None,
+                    ),
+                ])
+            )
+        )))
     }
 }
