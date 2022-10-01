c5ad8564854190f23a398240ecf9c2ccb91deca8
Julian Rosse <julian@helixbass.net>
2022-10-01 11:24:37 -0400

    rm unused

--- src/compiler/program/lines_0_500.rs
+++ src/compiler/program/lines_0_500.rs
@@ -1,8 +1,6 @@
-use regex::Regex;
-use std::borrow::Borrow;
-use std::cell::{Cell, Ref, RefCell, RefMut};
+use std::cell::{Ref, RefCell};
 use std::cmp;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::convert::TryInto;
 use std::io;
 use std::rc::Rc;
@@ -10,55 +8,14 @@ use std::time;
 use std::time::SystemTime;
 
 use crate::{
-    add_emit_flags, append, change_extension, clone, combine_paths, compare_paths, concatenate,
-    contains_path, convert_to_relative_path, create_diagnostic_collection,
-    create_diagnostic_for_node_in_source_file, create_get_canonical_file_name,
-    create_module_resolution_cache, create_multi_map, create_source_file, create_symlink_cache,
-    create_type_checker, create_type_reference_directive_resolution_cache,
-    diagnostic_category_name, extension_from_path, external_helpers_module_name_text,
-    file_extension_is, file_extension_is_one_of, flatten, for_each,
-    for_each_ancestor_directory_str, for_each_bool, for_each_child_returns, generate_djb2_hash,
-    get_allow_js_compiler_option, get_automatic_type_directive_names,
-    get_common_source_directory_of_config, get_default_lib_file_name, get_directory_path,
-    get_emit_module_resolution_kind, get_emit_script_target, get_external_module_name,
-    get_jsx_implicit_import_base, get_jsx_runtime_import, get_line_and_character_of_position,
-    get_new_line_character, get_normalized_absolute_path,
-    get_normalized_absolute_path_without_root, get_normalized_path_components,
-    get_output_declaration_file_name, get_package_scope_for_path, get_path_from_path_components,
-    get_property_assignment, get_spelling_suggestion, get_strict_option_value,
-    get_supported_extensions, get_supported_extensions_with_json_if_resolve_json_module, get_sys,
-    get_text_of_identifier_or_literal, has_extension, has_js_file_extension, has_jsdoc_nodes,
-    has_syntactic_modifier, is_ambient_module, is_any_import_or_re_export,
-    is_declaration_file_name, is_external_module, is_external_module_name_relative, is_import_call,
-    is_import_equals_declaration, is_in_js_file, is_literal_import_type_node,
-    is_module_declaration, is_require_call, is_rooted_disk_path, is_source_file_js,
-    is_string_literal, is_string_literal_like, is_watch_set, lib_map, libs, map_defined,
-    maybe_for_each, maybe_map, missing_file_modified_time, node_modules_path_part, normalize_path,
-    options_have_changes, out_file, package_id_to_string, remove_file_extension, remove_prefix,
-    remove_suffix, resolution_extension_is_ts_or_json, resolve_config_file_project_name,
-    resolve_module_name, resolve_type_reference_directive, set_parent, set_parent_recursive,
-    set_resolved_module, set_resolved_type_reference_directive,
-    source_file_affecting_compiler_options, stable_sort, starts_with, string_contains,
-    supported_js_extensions_flat, to_file_name_lower_case, to_path as to_path_helper,
-    walk_up_parenthesized_expressions, with_synthetic_factory_and_factory,
-    write_file_ensuring_directories, AutomaticTypeDirectiveFile, CancellationTokenDebuggable,
-    Comparison, CompilerHost, CompilerOptions, CompilerOptionsBuilder,
-    ConfigFileDiagnosticsReporter, CreateProgramOptions, CustomTransformers, Debug_, Diagnostic,
-    DiagnosticCollection, DiagnosticMessage, DiagnosticMessageText,
-    DiagnosticRelatedInformationInterface, Diagnostics, DirectoryStructureHost, EmitFlags,
-    EmitResult, Extension, FileIncludeKind, FileIncludeReason, FilePreprocessingDiagnostics,
-    FilePreprocessingDiagnosticsKind, FilePreprocessingFileExplainingDiagnostic,
-    FilePreprocessingReferencedDiagnostic, FileReference, LibFile, LineAndCharacter,
-    LiteralLikeNodeInterface, ModifierFlags, ModuleKind, ModuleResolutionCache,
-    ModuleResolutionHost, ModuleResolutionHostOverrider, ModuleResolutionKind,
-    ModuleSpecifierResolutionHost, MultiMap, NamedDeclarationInterface, Node, NodeArray, NodeFlags,
-    NodeInterface, PackageId, PackageJsonInfoCache, ParseConfigFileHost, ParseConfigHost,
-    ParsedCommandLine, Path, Program, ProjectReference, ReadonlyTextRange, RedirectTargetsMap,
-    ReferencedFile, ResolvedConfigFileName, ResolvedModuleFull, ResolvedProjectReference,
-    ResolvedTypeReferenceDirective, RootFile, ScriptReferenceHost, ScriptTarget, SortedArray,
-    SourceFile, SourceFileLike, SourceOfProjectReferenceRedirect, StringOrRcNode,
-    StructureIsReused, SymlinkCache, SyntaxKind, System, TypeChecker, TypeCheckerHost,
-    TypeCheckerHostDebuggable, TypeReferenceDirectiveResolutionCache, WriteFileCallback,
+    combine_paths, convert_to_relative_path, create_get_canonical_file_name, create_source_file,
+    diagnostic_category_name, for_each, for_each_ancestor_directory_str, generate_djb2_hash,
+    get_default_lib_file_name, get_directory_path, get_line_and_character_of_position,
+    get_new_line_character, get_normalized_path_components, get_path_from_path_components, get_sys,
+    is_rooted_disk_path, is_watch_set, missing_file_modified_time, normalize_path,
+    write_file_ensuring_directories, CompilerHost, CompilerOptions, Diagnostic,
+    DiagnosticMessageText, DiagnosticRelatedInformationInterface, LineAndCharacter,
+    ModuleResolutionHost, ModuleResolutionHostOverrider, Node, Path, ScriptTarget, System,
 };
 
 pub fn find_config_file<TFileExists: FnMut(&str) -> bool>(
