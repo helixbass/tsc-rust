5fc4f42f6649c5de30870dbfeb219e65915d88cf
Julian Rosse <julian@helixbass.net>
2022-10-06 17:46:00 -0400

    split file

--- src/compiler/emitter/mod.rs
+++ src/compiler/emitter/mod.rs
@@ -1,4 +1,6 @@
 mod lines_0_1000;
 pub use lines_0_1000::*;
-mod lines_1000_end;
-pub use lines_1000_end::*;
+mod lines_1000_1500;
+pub use lines_1000_1500::*;
+mod lines_1500_end;
+pub use lines_1500_end::*;
