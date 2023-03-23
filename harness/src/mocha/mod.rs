8f119b6c896b07b88f1c230e50a42659eb3d4a4e
Julian Rosse <julian@helixbass.net>
2023-03-23 10:22:25 -0400

    grep option

--- harness/src/mocha/mod.rs
+++ harness/src/mocha/mod.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use once_cell::sync::{Lazy, OnceCell};
+use regex::Regex;
 use std::any::Any;
 use std::cell::{Cell, RefCell};
 use std::io::{self, Write};
@@ -12,6 +13,9 @@ use threadpool::ThreadPool;
 
 #[derive(Clone, Debug, Parser)]
 pub struct MochaArgs {
+    #[arg(long)]
+    pub grep: Option<Regex>,
+
     #[arg(long)]
     pub start_index: Option<usize>,
 
@@ -200,7 +204,7 @@ pub fn describe(description: &str, callback: impl FnOnce()) {
         before()
     }
     for it in its {
-        if !should_run_it(&it) {
+        if !should_run_it(&it, &enclosing_descriptions) {
             continue;
         }
         let It {
@@ -217,7 +221,16 @@ pub fn describe(description: &str, callback: impl FnOnce()) {
     }
 }
 
-fn should_run_it(it: &It) -> bool {
+fn should_run_it(it: &It, enclosing_descriptions: &[String]) -> bool {
+    if let Some(config_grep) = config().grep.as_ref() {
+        if !(config_grep.is_match(&it.description)
+            || enclosing_descriptions
+                .into_iter()
+                .any(|enclosing_description| config_grep.is_match(enclosing_description)))
+        {
+            return false;
+        }
+    }
     if let Some(config_start_index) = config().start_index {
         if it.global_index < config_start_index {
             return false;
