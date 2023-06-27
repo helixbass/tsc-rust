3bc871434bfff4f929e5657e5bf61d17d7e01480
Julian Rosse <julian@helixbass.net>
2023-06-27 08:59:20 -0400

    try

--- typescript_rust/src/compiler/transformers/destructuring.rs
+++ typescript_rust/src/compiler/transformers/destructuring.rs
@@ -5,8 +5,8 @@ use gc::{Finalize, Gc, GcCell, Trace};
 use crate::{
     GetOrInsertDefault, Node, NodeExt, NodeInterface, NodeWrappered, NonEmpty, ReadonlyTextRange,
     TransformationContext, VisitResult, _d, is_destructuring_assignment, is_empty_array_literal,
-    is_empty_object_literal, is_expression, is_identifier, node_is_synthesized, visit_node,
-    BaseNodeFactory, Debug_, NodeFactory, ReadonlyTextRangeConcrete,
+    is_empty_object_literal, is_expression, is_identifier, node_is_synthesized, try_visit_node,
+    BaseNodeFactory, Debug_, NodeFactory, OptionTry, ReadonlyTextRangeConcrete,
 };
 
 trait FlattenContext {
@@ -24,7 +24,7 @@ trait FlattenContext {
         value: &Node,  /*Expression*/
         location: &(impl ReadonlyTextRange + ?Sized),
         original: Option<impl Borrow<Node>>,
-    );
+    ) -> io::Result<()>;
     fn create_array_binding_or_assignment_pattern(
         &self,
         elements: &[Gc<Node /*BindingOrAssignmentElement*/>],
@@ -37,7 +37,7 @@ trait FlattenContext {
         &self,
         node: &Node, /*Identifier*/
     ) -> Gc<Node /*BindingOrAssignmentElement*/>;
-    fn visitor(&self, _node: &Node) -> Option<VisitResult /*<Node>*/> {
+    fn visitor(&self, _node: &Node) -> Option<io::Result<VisitResult /*<Node>*/>> {
         None
     }
 }
@@ -88,117 +88,19 @@ pub fn flatten_destructuring_assignment<'visitor, 'create_assignment_callback>(
             + 'create_assignment_callback,
     >,
 ) -> Gc<Node /*Expression*/> {
-    let mut location/*: TextRange*/ = node.node_wrapper();
-    let mut value: Option<Gc<Node /*Expression*/>> = _d();
-    let mut node = node.node_wrapper();
-    let visitor: Option<Rc<RefCell<dyn FnMut(&Node) -> VisitResult + 'visitor>>> =
-        visitor.map(|visitor| {
-            Rc::new(RefCell::new(visitor))
-                as Rc<RefCell<dyn FnMut(&Node) -> VisitResult + 'visitor>>
-        });
-    let create_assignment_callback: Option<
-        Rc<
-            RefCell<
-                dyn FnMut(&Node, &Node, Option<&dyn ReadonlyTextRange>) -> Gc<Node>
-                    + 'create_assignment_callback,
-            >,
-        >,
-    > = create_assignment_callback.map(|create_assignment_callback| {
-        Rc::new(RefCell::new(create_assignment_callback))
-            as Rc<
-                RefCell<
-                    dyn FnMut(&Node, &Node, Option<&dyn ReadonlyTextRange>) -> Gc<Node>
-                        + 'create_assignment_callback,
-                >,
-            >
-    });
-    if is_destructuring_assignment(&node) {
-        value = Some(node.as_binary_expression().right.clone());
-        while is_empty_array_literal(&node.as_binary_expression().left)
-            || is_empty_object_literal(&node.as_binary_expression().left)
-        {
-            if is_destructuring_assignment(value.as_ref().unwrap()) {
-                node = value.clone().unwrap();
-                location = value.clone().unwrap();
-                value = Some(node.as_binary_expression().right.clone());
-            } else {
-                return visit_node(
-                    value.as_ref().unwrap(),
-                    visitor
-                        .as_ref()
-                        .map(|visitor| |node: &Node| (visitor.borrow_mut())(node)),
-                    Some(is_expression),
-                    Option::<fn(&[Gc<Node>]) -> Gc<Node>>::None,
-                );
-            }
-        }
-    }
-
-    let expressions: Gc<GcCell<Option<Vec<Gc<Node /*Expression*/>>>>> = _d();
-    let flatten_context = FlattenDestructuringAssignmentFlattenContext::new(
-        context.clone(),
+    try_flatten_destructuring_assignment(
+        node,
+        visitor.map(|mut visitor| move |node: &Node| Ok(visitor(node))),
+        context,
         level,
-        context.get_compiler_options().downlevel_iteration == Some(true),
-        expressions.clone(),
-        create_assignment_callback.clone(),
-        visitor.clone(),
-    );
-
-    if value.is_some() {
-        value = Some(visit_node(
-            value.as_ref().unwrap(),
-            visitor
-                .as_ref()
-                .map(|visitor| |node: &Node| (visitor.borrow_mut())(node)),
-            Some(is_expression),
-            Option::<fn(&[Gc<Node>]) -> Gc<Node>>::None,
-        ));
-
-        if is_identifier(value.as_ref().unwrap())
-            && binding_or_assignment_element_assigns_to_name(
-                &node,
-                &value.as_ref().unwrap().as_identifier().escaped_text,
-            )
-            || binding_or_assignment_element_contains_non_literal_computed_name(&node)
-        {
-            value = Some(ensure_identifier(
-                &flatten_context,
-                value.as_ref().unwrap(),
-                false,
-                &*location,
-            ));
-        } else if needs_value == Some(true) {
-            value = Some(ensure_identifier(
-                &flatten_context,
-                value.as_ref().unwrap(),
-                true,
-                &*location,
-            ));
-        } else if node_is_synthesized(&*node) {
-            location = value.clone().unwrap();
-        }
-    }
-
-    flatten_binding_or_assignment_element(
-        &flatten_context,
-        &node,
-        value.as_deref(),
-        &*location,
-        Some(is_destructuring_assignment(&node)),
-    );
-
-    if let Some(value) = value.filter(|_| needs_value == Some(true)) {
-        if !(*expressions).borrow().as_ref().is_non_empty() {
-            return value;
-        }
-
-        expressions.borrow_mut().as_mut().unwrap().push(value);
-    }
-
-    let ret = context
-        .factory()
-        .inline_expressions((*expressions).borrow().as_ref().unwrap()) /* || context.factory.createOmittedExpression()*/;
-    ret
+        needs_value,
+        create_assignment_callback.map(|mut create_assignment_callback| {
+            move |a: &Node, b: &Node, c: Option<&dyn ReadonlyTextRange>| {
+                Ok(create_assignment_callback(a, b, c))
+            }
+        }),
+    )
+    .unwrap()
 }
 
 fn emit_expression(
@@ -216,12 +118,12 @@ struct FlattenDestructuringAssignmentFlattenContext<'visitor, 'create_assignment
     create_assignment_callback: Option<
         Rc<
             RefCell<
-                dyn FnMut(&Node, &Node, Option<&dyn ReadonlyTextRange>) -> Gc<Node>
+                dyn FnMut(&Node, &Node, Option<&dyn ReadonlyTextRange>) -> io::Result<Gc<Node>>
                     + 'create_assignment_callback,
             >,
         >,
     >,
-    visitor: Option<Rc<RefCell<dyn FnMut(&Node) -> VisitResult + 'visitor>>>,
+    visitor: Option<Rc<RefCell<dyn FnMut(&Node) -> io::Result<VisitResult> + 'visitor>>>,
 }
 
 impl<'visitor, 'create_assignment_callback>
@@ -235,12 +137,12 @@ impl<'visitor, 'create_assignment_callback>
         create_assignment_callback: Option<
             Rc<
                 RefCell<
-                    dyn FnMut(&Node, &Node, Option<&dyn ReadonlyTextRange>) -> Gc<Node>
+                    dyn FnMut(&Node, &Node, Option<&dyn ReadonlyTextRange>) -> io::Result<Gc<Node>>
                         + 'create_assignment_callback,
                 >,
             >,
         >,
-        visitor: Option<Rc<RefCell<dyn FnMut(&Node) -> VisitResult + 'visitor>>>,
+        visitor: Option<Rc<RefCell<dyn FnMut(&Node) -> io::Result<VisitResult> + 'visitor>>>,
     ) -> Self {
         Self {
             context,
@@ -280,7 +182,7 @@ impl FlattenContext for FlattenDestructuringAssignmentFlattenContext<'_, '_> {
         value: &Node,  /*Expression*/
         location: &(impl ReadonlyTextRange + ?Sized),
         original: Option<impl Borrow<Node>>,
-    ) {
+    ) -> io::Result<()> {
         Debug_.assert_node(
             Some(target),
             Some(if self.create_assignment_callback.is_some() {
@@ -293,22 +195,23 @@ impl FlattenContext for FlattenDestructuringAssignmentFlattenContext<'_, '_> {
         let expression = self
             .create_assignment_callback
             .as_ref()
-            .map_or_else(
-                || {
-                    self.context
+            .try_map_or_else(
+                || -> io::Result<_> {
+                    Ok(self
+                        .context
                         .factory()
                         .create_assignment(
-                            visit_node(
+                            try_visit_node(
                                 target,
                                 self.visitor
                                     .as_ref()
                                     .map(|visitor| |node: &Node| (visitor.borrow_mut())(node)),
                                 Some(is_expression),
                                 Option::<fn(&[Gc<Node>]) -> Gc<Node>>::None,
-                            ),
+                            )?,
                             value.node_wrapper(),
                         )
-                        .set_text_range(Some(location))
+                        .set_text_range(Some(location)))
                 },
                 |create_assignment_callback| {
                     (create_assignment_callback.borrow_mut())(
@@ -317,9 +220,11 @@ impl FlattenContext for FlattenDestructuringAssignmentFlattenContext<'_, '_> {
                         Some(&ReadonlyTextRangeConcrete::from(location)),
                     )
                 },
-            )
+            )?
             .and_set_original(original.node_wrappered());
         self.emit_expression(&expression);
+
+        Ok(())
     }
 
     fn create_array_binding_or_assignment_pattern(
@@ -343,29 +248,140 @@ impl FlattenContext for FlattenDestructuringAssignmentFlattenContext<'_, '_> {
         make_assignment_element(node)
     }
 
-    fn visitor(&self, node: &Node) -> Option<VisitResult /*<Node>*/> {
+    fn visitor(&self, node: &Node) -> Option<io::Result<VisitResult /*<Node>*/>> {
         self.visitor
             .as_ref()
             .map(|visitor| (visitor.borrow_mut())(node))
     }
 }
 
-pub fn try_flatten_destructuring_assignment(
-    _node: &Node, /*VariableDeclaration | DestructuringAssignment*/
-    _visitor: Option<impl FnMut(&Node) -> io::Result<VisitResult>>,
-    _context: Gc<Box<dyn TransformationContext>>,
-    _level: FlattenLevel,
-    _needs_value: Option<bool>,
+pub fn try_flatten_destructuring_assignment<'visitor, 'create_assignment_callback>(
+    node: &Node, /*VariableDeclaration | DestructuringAssignment*/
+    visitor: Option<impl FnMut(&Node) -> io::Result<VisitResult> + 'visitor>,
+    context: Gc<Box<dyn TransformationContext>>,
+    level: FlattenLevel,
+    needs_value: Option<bool>,
     // create_assignment_callback: Option<Gc<Box<dyn TryCreateAssignmentCallback>>>,
-    _create_assignment_callback: Option<
+    create_assignment_callback: Option<
         impl FnMut(
-            &Node, /*Identifier*/
-            &Node, /*Expression*/
-            Option<&dyn ReadonlyTextRange>,
-        ) -> io::Result<Gc<Node /*Expression*/>>,
+                &Node, /*Identifier*/
+                &Node, /*Expression*/
+                Option<&dyn ReadonlyTextRange>,
+            ) -> io::Result<Gc<Node /*Expression*/>>
+            + 'create_assignment_callback,
     >,
 ) -> io::Result<Gc<Node /*Expression*/>> {
-    unimplemented!()
+    let mut location/*: TextRange*/ = node.node_wrapper();
+    let mut value: Option<Gc<Node /*Expression*/>> = _d();
+    let mut node = node.node_wrapper();
+    let visitor: Option<Rc<RefCell<dyn FnMut(&Node) -> io::Result<VisitResult> + 'visitor>>> =
+        visitor.map(|visitor| {
+            Rc::new(RefCell::new(visitor))
+                as Rc<RefCell<dyn FnMut(&Node) -> io::Result<VisitResult> + 'visitor>>
+        });
+    let create_assignment_callback: Option<
+        Rc<
+            RefCell<
+                dyn FnMut(&Node, &Node, Option<&dyn ReadonlyTextRange>) -> io::Result<Gc<Node>>
+                    + 'create_assignment_callback,
+            >,
+        >,
+    > = create_assignment_callback.map(|create_assignment_callback| {
+        Rc::new(RefCell::new(create_assignment_callback))
+            as Rc<
+                RefCell<
+                    dyn FnMut(&Node, &Node, Option<&dyn ReadonlyTextRange>) -> io::Result<Gc<Node>>
+                        + 'create_assignment_callback,
+                >,
+            >
+    });
+    if is_destructuring_assignment(&node) {
+        value = Some(node.as_binary_expression().right.clone());
+        while is_empty_array_literal(&node.as_binary_expression().left)
+            || is_empty_object_literal(&node.as_binary_expression().left)
+        {
+            if is_destructuring_assignment(value.as_ref().unwrap()) {
+                node = value.clone().unwrap();
+                location = value.clone().unwrap();
+                value = Some(node.as_binary_expression().right.clone());
+            } else {
+                return try_visit_node(
+                    value.as_ref().unwrap(),
+                    visitor
+                        .as_ref()
+                        .map(|visitor| |node: &Node| (visitor.borrow_mut())(node)),
+                    Some(is_expression),
+                    Option::<fn(&[Gc<Node>]) -> Gc<Node>>::None,
+                );
+            }
+        }
+    }
+
+    let expressions: Gc<GcCell<Option<Vec<Gc<Node /*Expression*/>>>>> = _d();
+    let flatten_context = FlattenDestructuringAssignmentFlattenContext::new(
+        context.clone(),
+        level,
+        context.get_compiler_options().downlevel_iteration == Some(true),
+        expressions.clone(),
+        create_assignment_callback.clone(),
+        visitor.clone(),
+    );
+
+    if value.is_some() {
+        value = Some(try_visit_node(
+            value.as_ref().unwrap(),
+            visitor
+                .as_ref()
+                .map(|visitor| |node: &Node| (visitor.borrow_mut())(node)),
+            Some(is_expression),
+            Option::<fn(&[Gc<Node>]) -> Gc<Node>>::None,
+        )?);
+
+        if is_identifier(value.as_ref().unwrap())
+            && binding_or_assignment_element_assigns_to_name(
+                &node,
+                &value.as_ref().unwrap().as_identifier().escaped_text,
+            )
+            || binding_or_assignment_element_contains_non_literal_computed_name(&node)
+        {
+            value = Some(ensure_identifier(
+                &flatten_context,
+                value.as_ref().unwrap(),
+                false,
+                &*location,
+            ));
+        } else if needs_value == Some(true) {
+            value = Some(ensure_identifier(
+                &flatten_context,
+                value.as_ref().unwrap(),
+                true,
+                &*location,
+            ));
+        } else if node_is_synthesized(&*node) {
+            location = value.clone().unwrap();
+        }
+    }
+
+    flatten_binding_or_assignment_element(
+        &flatten_context,
+        &node,
+        value.as_deref(),
+        &*location,
+        Some(is_destructuring_assignment(&node)),
+    );
+
+    if let Some(value) = value.filter(|_| needs_value == Some(true)) {
+        if !(*expressions).borrow().as_ref().is_non_empty() {
+            return Ok(value);
+        }
+
+        expressions.borrow_mut().as_mut().unwrap().push(value);
+    }
+
+    let ret = context
+        .factory()
+        .inline_expressions((*expressions).borrow().as_ref().unwrap()) /* || context.factory.createOmittedExpression()*/;
+    Ok(ret)
 }
 
 fn binding_or_assignment_element_assigns_to_name(
