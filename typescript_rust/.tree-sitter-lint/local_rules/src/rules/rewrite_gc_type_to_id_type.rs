f73dba3ded7efc6f4aed3794a9f0902cc3e1653a
Julian Rosse <julian@helixbass.net>
2023-12-08 12:28:53 -0500

    rule for type mapper

--- typescript_rust/.tree-sitter-lint/local_rules/src/rules/rewrite_gc_type_to_id_type.rs
+++ typescript_rust/.tree-sitter-lint/local_rules/src/rules/rewrite_gc_type_to_id_type.rs
@@ -11,7 +11,7 @@ pub fn rewrite_gc_type_to_id_type_rule() -> Arc<dyn Rule> {
               (generic_type
                  type: (type_identifier) @gc (#eq? @gc "Gc")
                  type_arguments: (type_arguments
-                   (type_identifier) @type (#eq? @type "Type")
+                   (type_identifier) @type (#eq? @type "TypeMapper")
                  )
               )
             "# => {
@@ -19,7 +19,7 @@ pub fn rewrite_gc_type_to_id_type_rule() -> Arc<dyn Rule> {
                 callback => |node, context| {
                     context.report(
                         violation! {
-                            message => "Gc<Type> -> Id<Type>",
+                            message => "Gc<TypeMapper> -> Id<TypeMapper>",
                             node => node,
                             fix => |fixer| {
                                 fixer.replace_text(node, "Id");
