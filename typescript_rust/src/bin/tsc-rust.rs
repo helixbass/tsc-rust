d9422bfbeb47bd7945da0c679fdc7ee54748ea3b
Julian Rosse <julian@helixbass.net>
2024-01-24 17:27:42 -0500

    working on system

--- typescript_rust/src/bin/tsc-rust.rs
+++ typescript_rust/src/bin/tsc-rust.rs
@@ -1,7 +1,7 @@
 use std::{io, rc::Rc};
 
 use regex::Regex;
-use typescript_rust::{execute_command_line, get_sys, Debug_, LogLevel, LoggingHost, static_arena, HasArena, AllArenas};
+use typescript_rust::{execute_command_line, get_sys, Debug_, LogLevel, LoggingHost, static_arena, HasArena, AllArenas, InArena};
 
 fn main() -> io::Result<()> {
     Debug_.set_logging_host(Some(Rc::new(LoggingHostConcrete::new())));
@@ -10,25 +10,25 @@ fn main() -> io::Result<()> {
         Debug_.enable_debug_info();
     }
 
-    let arena = static_arena();
+    let arena = &*static_arena();
 
-    let sys = get_sys(&*arena);
+    let sys = get_sys(arena);
 
     if
     /*sys.tryEnableSourceMapsForHost &&*/
     Regex::new(r#"(?i)^development$"#)
         .unwrap()
         // TODO: does using NODE_ENV make sense?
-        .is_match(&sys.get_environment_variable("NODE_ENV"))
+        .is_match(&sys.ref_(arena).get_environment_variable("NODE_ENV"))
     {
-        sys.try_enable_source_maps_for_host();
+        sys.ref_(arena).try_enable_source_maps_for_host();
     }
 
     // if (ts.sys.setBlocking) {
-    sys.set_blocking();
+    sys.ref_(arena).set_blocking();
     // }
 
-    execute_command_line(sys.clone(), |_| {}, sys.args(), &*arena)?;
+    execute_command_line(sys.clone(), |_| {}, sys.ref_(arena).args(), arena)?;
 
     Ok(())
 }
@@ -44,7 +44,7 @@ impl LoggingHostConcrete {
 impl LoggingHost for LoggingHostConcrete {
     fn log(&self, _level: LogLevel, s: &str) {
         let sys = get_sys(self);
-        sys.write(&format!("{}{}", s /*|| ""*/, sys.new_line()));
+        sys.ref_(self).write(&format!("{}{}", s /*|| ""*/, sys.ref_(self).new_line()));
     }
 }
 
