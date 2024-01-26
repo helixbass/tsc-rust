82f7fa62e13b8e30b93999b2930e994c64ef9021
Julian Rosse <julian@helixbass.net>
2024-01-26 13:42:18 -0500

    working on resolved type reference directive

--- typescript_rust/src/compiler/checker/lines_43500_end.rs
+++ typescript_rust/src/compiler/checker/lines_43500_end.rs
@@ -990,7 +990,7 @@ impl EmitResolverCreateResolver {
                     return;
                 }
                 let resolved_directive = resolved_directive.clone().unwrap();
-                let resolved_directive_ref = resolved_directive.ref_(self);
+                let resolved_directive_ref = resolved_directive.ref_(arena);
                 let resolved_directive_resolved_file_name = resolved_directive_ref.resolved_file_name.as_ref();
                 if resolved_directive_resolved_file_name.non_empty().is_none() {
                     return;
