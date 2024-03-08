82faf7dd2cb8ca121fec4aad4c5c5ecf5ca4c6f1
Julian Rosse <julian@helixbass.net>
2024-03-08 14:13:43 -0500

    working on types and symbols

--- harness/src/harness/type_writer.rs
+++ harness/src/harness/type_writer.rs
@@ -1,13 +1,21 @@
+use std::{cell::RefCell, collections::HashMap};
+
+use squalid::regex;
 use typescript_rust::{
     id_arena::Id, AllArenas, HasArena, InArena, Node, Program, SyntaxKind, TypeChecker, _d,
-    for_each_child, get_source_text_of_node_from_source_file, impl_has_arena, is_binding_element,
-    is_declaration_name, is_expression_node,
-    is_expression_with_type_arguments_in_class_extends_clause, is_global_scope_augmentation,
-    is_identifier, is_meta_property, is_module_declaration, is_part_of_type_node,
-    is_property_access_or_qualified_name, skip_trivia, NodeArray, NodeInterface, ReadonlyTextRange,
-    ScriptReferenceHost, SourceFileLike, TypeFlags, TypeFormatFlags,
+    for_each_child, get_base_file_name, get_source_text_of_node_from_source_file, impl_has_arena,
+    is_binding_element, is_declaration_name, is_export_assignment, is_export_specifier,
+    is_expression_node, is_expression_with_type_arguments_in_class_extends_clause,
+    is_global_scope_augmentation, is_identifier, is_import_clause, is_import_equals_declaration,
+    is_import_specifier, is_intrinsic_jsx_name, is_jsx_closing_element, is_jsx_opening_element,
+    is_jsx_self_closing_element, is_meta_property, is_module_declaration, is_part_of_type_node,
+    is_property_access_or_qualified_name, is_type_alias_declaration, skip_trivia,
+    NamedDeclarationInterface, NodeArray, NodeInterface, ReadonlyTextRange, ScriptReferenceHost,
+    SourceFileLike, SymbolInterface, TypeFlags, TypeFormatFlags, TypeInterface,
 };
-use typescript_services_rust::{get_meaning_from_declaration, is_label_name, SemanticMeaning};
+use typescript_services_rust::{
+    get_meaning_from_declaration, is_label_name, NodeServicesInterface, SemanticMeaning,
+};
 
 pub struct TypeWriterTypeResult {
     pub line: usize,
@@ -141,6 +149,91 @@ impl TypeWriterWalker {
         ret
     }
 
+    fn is_import_statement_name(&self, node: Id<Node>) -> bool {
+        if is_import_specifier(&node.ref_(self).parent().ref_(self))
+            && (node
+                .ref_(self)
+                .parent()
+                .ref_(self)
+                .as_import_specifier()
+                .name
+                == node
+                || node
+                    .ref_(self)
+                    .parent()
+                    .ref_(self)
+                    .as_import_specifier()
+                    .property_name
+                    == Some(node))
+        {
+            return true;
+        }
+        if is_import_clause(&node.ref_(self).parent().ref_(self))
+            && node.ref_(self).parent().ref_(self).as_import_clause().name == Some(node)
+        {
+            return true;
+        }
+        if is_import_equals_declaration(&node.ref_(self).parent().ref_(self))
+            && node
+                .ref_(self)
+                .parent()
+                .ref_(self)
+                .as_import_equals_declaration()
+                .name()
+                == node
+        {
+            return true;
+        }
+        false
+    }
+
+    fn is_export_statement_name(&self, node: Id<Node>) -> bool {
+        if is_export_assignment(&node.ref_(self).parent().ref_(self))
+            && node
+                .ref_(self)
+                .parent()
+                .ref_(self)
+                .as_export_assignment()
+                .expression
+                == node
+        {
+            return true;
+        }
+        if is_export_specifier(&node.ref_(self).parent().ref_(self))
+            && (node
+                .ref_(self)
+                .parent()
+                .ref_(self)
+                .as_export_specifier()
+                .name
+                == node
+                || node
+                    .ref_(self)
+                    .parent()
+                    .ref_(self)
+                    .as_export_specifier()
+                    .property_name
+                    == Some(node))
+        {
+            return true;
+        }
+        false
+    }
+
+    fn is_intrinsic_jsx_tag(&self, node: Id<Node>) -> bool {
+        let p = node.ref_(self).parent();
+        if !(is_jsx_opening_element(&p.ref_(self))
+            || is_jsx_closing_element(&p.ref_(self))
+            || is_jsx_self_closing_element(&p.ref_(self)))
+        {
+            return false;
+        }
+        if p.ref_(self).as_has_tag_name().tag_name() != node {
+            return false;
+        }
+        is_intrinsic_jsx_name(node.ref_(self).get_text())
+    }
+
     fn write_type_or_symbol(
         &self,
         node: Id<Node>,
@@ -168,9 +261,9 @@ impl TypeWriterWalker {
         if !is_symbol_walk {
             if is_part_of_type_node(node, self)
                 || is_identifier(&node.ref_(self))
-                    && !get_meaning_from_declaration(node.ref_(self).parent())
+                    && !get_meaning_from_declaration(node.ref_(self).parent(), self)
                         .intersects(SemanticMeaning::Value)
-                    && !(is_type_alias_declaration(&node.ref_(self).parent())
+                    && !(is_type_alias_declaration(&node.ref_(self).parent().ref_(self))
                         && node
                             .ref_(self)
                             .parent()
@@ -212,21 +305,99 @@ impl TypeWriterWalker {
                     .intrinsic_name()
                     .to_owned()
             } else {
-                self.checker.ref_(self).type_to_string(
-                    type_,
-                    Some(node.ref_(self).parent()),
-                    Some(TypeFormatFlags::NoTruncation | TypeFormatFlags::AllowUniqueESSymbolType),
-                )
+                self.checker
+                    .ref_(self)
+                    .type_to_string(
+                        type_,
+                        Some(node.ref_(self).parent()),
+                        Some(
+                            TypeFormatFlags::NoTruncation
+                                | TypeFormatFlags::AllowUniqueESSymbolType,
+                        ),
+                    )
+                    .unwrap()
             };
             return Some(TypeWriterResult::Type(TypeWriterTypeResult {
                 line: line_and_character.line,
                 syntax_kind: node.ref_(self).kind(),
-                source_text,
+                source_text: source_text.into_owned(),
                 type_: type_string,
             }));
         }
-
-        unimplemented!()
+        let symbol = self
+            .checker
+            .ref_(self)
+            .get_symbol_at_location(node)
+            .unwrap()?;
+        let mut symbol_string = format!(
+            "Symbol({}",
+            self.checker
+                .ref_(self)
+                .symbol_to_string(symbol, None, None, None)
+                .unwrap(),
+        );
+        if let Some(symbol_declarations) = symbol.ref_(self).maybe_declarations().as_ref() {
+            let mut count = 0;
+            for &declaration in symbol_declarations {
+                if count >= 5 {
+                    symbol_string.push_str(&format!(
+                        " ... and {} more",
+                        symbol_declarations.len() - count
+                    ));
+                    break;
+                }
+                count += 1;
+                symbol_string.push_str(", ");
+                thread_local! {
+                    static SYMBOL_TEST_OUTPUT_CACHE: RefCell<HashMap<Id<Node>, String>> = RefCell::new(HashMap::new());
+                }
+                symbol_string.push_str(&SYMBOL_TEST_OUTPUT_CACHE.with(
+                    |symbol_test_output_cache| {
+                        symbol_test_output_cache
+                            .borrow_mut()
+                            .entry(declaration)
+                            .or_insert_with(|| {
+                                let decl_source_file = declaration.ref_(self).get_source_file(self);
+                                let decl_line_and_character = decl_source_file
+                                    .ref_(self)
+                                    .as_source_file()
+                                    .get_line_and_character_of_position(
+                                        usize::try_from(declaration.ref_(self).pos()).unwrap(),
+                                    );
+                                let file_name = get_base_file_name(
+                                    &decl_source_file.ref_(self).as_source_file().file_name(),
+                                    None,
+                                    None,
+                                );
+                                let is_lib_file =
+                                    regex!(r#"(?i)lib(.*)\.d\.ts"#).is_match(&file_name);
+                                format!(
+                                    "Decl({}, {}, {})",
+                                    file_name,
+                                    if is_lib_file {
+                                        "--".to_owned()
+                                    } else {
+                                        decl_line_and_character.line.to_string()
+                                    },
+                                    if is_lib_file {
+                                        "--".to_owned()
+                                    } else {
+                                        decl_line_and_character.character.to_string()
+                                    },
+                                )
+                            })
+                            .clone()
+                    },
+                ));
+            }
+        }
+        symbol_string.push_str(")");
+        Some(TypeWriterResult::Symbol(TypeWriterSymbolResult {
+            line: line_and_character.line,
+            syntax_kind: node.ref_(self).kind(),
+            source_text: source_text.into_owned(),
+            symbol: symbol_string,
+        }))
     }
 }
 
